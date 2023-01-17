1d3bf7b3b6866f9bfaebcfdfddc3300228a1adff
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2023-01-17 13:09:09 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 5932 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.cc
+++ 백준/Silver/10845. 큐/큐.cc
@@ -1,41 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int MX = 1000005;
+int dat[MX];
+int head, tail = 0;
+
+void push(int num)
+{
+    dat[tail++] = num;
+}
+
+void pop()
+{
+    head++;
+}
+
+int front()
+{
+    return dat[head]; 
+}
+
+int back()
+{
+    return dat[tail - 1];
+}
 int main(){
     ios::sync_with_stdio(0), cin.tie(0);
-
-    queue<int> Q;
-    int n, m;
+    
+    int n, num;
     string init;
 
     cin >> n;
     while(n--)
     {
         cin >> init;
+
         if(init == "push")
         {
-            cin >> m;
-            Q.push(m);
+            cin >> num;
+            push(num);
         }
         else if(init == "pop")
         {
-            if(Q.empty()) cout << -1 << '\n';
-            else {
-                cout << Q.front() << '\n';
-                Q.pop();
+            if(tail - head == 0) cout << -1 << '\n';
+            else 
+            {
+                cout << front() <<'\n';
+                pop();
             }
         }
-        else if(init == "size") cout << Q.size() << '\n';
-        else if(init == "empty") cout << (int)Q.empty() << '\n';
+        else if(init == "size") cout << tail - head << '\n';
+        else if(init == "empty")
+        {
+            if(tail - head == 0) cout << 1 << '\n';
+            else cout << 0 << '\n';
+        }
         else if(init == "front")
         {
-            if(Q.empty()) cout << -1 << '\n';
-            else cout << Q.front() << '\n';
+            if(tail - head == 0) cout << -1 << '\n';
+            else cout << front() << '\n';
         }
-        else // back
+        else if(init == "back")
         {
-            if(Q.empty()) cout << -1 <<'\n';
-            else cout << Q.back() << '\n';
+            if(tail - head == 0) cout << -1 << '\n';
+            else cout << back() << '\n';
         }
-    }
+    } // O(N)
 }
