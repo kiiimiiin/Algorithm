f0ba8a417085e9d7461208ce8174b9c80f95dbed
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2023-01-23 17:12:22 +0900

    [Gold IV] Title: 불!, Time: 48 ms, Memory: 10956 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -3,88 +3,73 @@ using namespace std;
 #define X first
 #define Y second
 
-// 지훈, 불 따로 bfs 
-// 지훈이는 불이 탄 지점을 가지 못함 
-// bfs를 다 돌리고 지훈이 dist가 모서리에 존재하면 탈출 성공 아니면 실패 
-
 string board[1002];
-int distJ[1002][1002], distF[1002][1002];
-int r,c;
-int dx[4]  = { 1, 0 , -1, 0};
-int dy[4] = { 0, 1, 0, -1 };
+int dist1[1002][1002]; // 불 거리(시간)
+int dist2[1002][1002]; // 지훈 거리(시간)
+int r, c;
+int dx[4] = { -1 , 0, 1 , 0};
+int dy[4] = { 0, 1, 0, -1};
 
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0);
-    cin >> r >> c;
-    for(int i = 0; i < r; i++) cin >> board[i];
-    for(int i = 0; i < r; i++) 
-    {
-        fill(distJ[i], distJ[i] + c, -1);
-        fill(distF[i], distF[i] + c, -1); // 방문이력 없는 곳
-    }
-    
-    queue<pair<int, int>> QJ, QF;
 
-    for(int i = 0; i < r; i++)
+    cin >> r >> c ;
+    
+    for(int i = 0 ; i < r ; i++) cin >> board[i];
+    for(int i = 0 ; i < r ; i++)
     {
-        for(int j = 0; j < c; j++)
+        fill(dist1[i], dist1[i] + c , -1);
+        fill(dist2[i], dist2[i] + c , -1); // 방문 이력 없음
+    }
+
+    queue<pair<int,int>> Q1,Q2;
+    for(int i = 0; i < r ; i++)
+    {
+        for(int j = 0 ; j < c ; j++)
         {
-            if(board[i][j] == 'J')
-            {
-                distJ[i][j] = 0; 
-                QJ.push({i,j}); // 지훈 시작점
-            }
             if(board[i][j] == 'F')
             {
-                distF[i][j] = 0; 
-                QF.push({i,j}); // 불 시작점
+                dist1[i][j] = 0;
+                Q1.push({i,j}); // 불 시작점
+            }
+            if(board[i][j] == 'J')
+            {
+                dist2[i][j] = 0;
+                Q2.push({i,j}); // 지훈 시작점
             }
         }
     }
-    while(!QF.empty())
-    { // 불을 먼저 bfs 돌려서 지훈이 갈 수 있는 지점을 만듦
-        auto cur = QF.front(); QF.pop();
+    while(!Q1.empty())
+    {
+        auto cur = Q1.front(); Q1.pop();
         for(int dir = 0; dir < 4 ; dir++)
         {
             int nx = cur.X + dx[dir];
             int ny = cur.Y + dy[dir];
             if(nx < 0 || nx >= r || ny < 0 || ny >= c) continue;
-            if(board[nx][ny] == '#' || distF[nx][ny] >= 0) continue;
-            distF[nx][ny] = distF[cur.X][cur.Y] + 1;
-            QF.push({nx,ny});
+            if(board[nx][ny] == '#' || dist1[nx][ny] >= 0) continue;
+            dist1[nx][ny] = dist1[cur.X][cur.Y] + 1;
+            Q1.push({nx,ny});
         }
     }
-    while(!QJ.empty())
-    { // 지훈이가 불이 지나간 곳을 피하며 탈출 지점으로 이동
-        auto cur = QJ.front(); QJ.pop();
-       for(int dir = 0; dir < 4 ; dir++)
+    while(!Q2.empty())
+    {
+        auto cur = Q2.front(); Q2.pop();
+        for(int dir = 0; dir < 4 ; dir++)
         {
             int nx = cur.X + dx[dir];
             int ny = cur.Y + dy[dir];
-            if(nx < 0 || nx >= r || ny < 0 || ny >= c) continue;
-            if(board[nx][ny] == '#' || distJ[nx][ny] >= 0) continue;
-            if(distJ[cur.X][cur.Y] + 1 >= distF[nx][ny] && distF[nx][ny] > -1) continue; // 불이 먼저 지나간 곳은 못감
-            distJ[nx][ny] = distJ[cur.X][cur.Y] + 1;
-            QJ.push({nx,ny});
+            if(nx < 0 || nx >= r || ny < 0 || ny >= c)
+            { // 거리순으로 탐색하므로 가장 먼저 탈출했을 때의 시간 출력
+                cout << dist2[cur.X][cur.Y] + 1;
+                return 0;
+            }
+            if(board[nx][ny] == '#' || dist2[nx][ny] >= 0) continue;
+            if(dist2[cur.X][cur.Y] + 1 >= dist1[nx][ny] && dist1[nx][ny] != -1) continue; // 불이 붙은 곳 제외
+            dist2[nx][ny] = dist2[cur.X][cur.Y] + 1;
+            Q2.push({nx,ny});
         }
     }
-    
-    int minTime = r * c + 1; 
-
-    for(int i = 0; i < r; i++) // 왼쪽, 오른쪽 도착 여부
-    {   
-        if(distJ[i][0] != -1) minTime = min(distJ[i][0], minTime);
-        if(distJ[i][c-1] != -1) minTime = min(distJ[i][c-1], minTime);
-    }
-    //불이 없을 때 이상함 
-
-    for(int j = 0; j < c; j++) // 위쪽, 아래쪽 도착 여부 
-    {
-        if(distJ[0][j] != -1) minTime = min(distJ[0][j], minTime);
-        if(distJ[r-1][j] != -1) minTime = min(distJ[r-1][j], minTime); 
-    }
-
-    if(minTime < r * c + 1) cout << minTime + 1; // 가장자리에서 한 칸 더 이동하면 탈출
-    else cout << "IMPOSSIBLE";
+    cout << "IMPOSSIBLE"; // 지훈이 BFS를 다 돌아도 탈출하지 못함.
 }
