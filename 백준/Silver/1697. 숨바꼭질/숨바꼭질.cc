1bae20cb8067b000ce7a95dbe13434cf34ab711c
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2023-01-25 00:41:12 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2544 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,35 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int dist[100001];
-int n, k;
-
+#define X first
+#define Y second
+int dist[100002];
+int n,k;
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0);
     cin >> n >> k;
-    if(n == k) cout << 0;
-    fill(dist, dist+100001, -1); // 방문 이력 없는 곳 
-    dist[k] = -2; // 동생위치
-    
+    fill(dist, dist+100001, -1); //방문 이력 없음
     queue<int> Q;
     dist[n] = 0;
-    Q.push(n); // 수빈 시작점
-    
-    while(!Q.empty())
+    Q.push(n); // 수진 시작점
+    while(dist[k] == -1) // 동생 위치를 차지하기 전까지 BFS
     {
-        auto cur = Q.front(); Q.pop();
-        for(int dir = -1; dir < 2 ; dir++)
-        {   
-            int nx;
-            if( dir == 0)   nx = cur * 2; // 순간 이동
-            else nx = cur + dir;  // 좌 우 이동
-
-            if( nx < 0 || nx >= 100001) continue;
-            if( dist[nx] >= 0) continue;
-            else if( dist[nx] == -2) cout << dist[cur] + 1; // 동생 찾음
-            dist[nx] = dist[cur] + 1;
-            Q.push(nx);
+        int cur = Q.front(); Q.pop();
+        for(int nxt : {cur-1, cur+1, 2*cur}) // range-based for
+        {
+            if(nxt < 0 || nxt > 100000) continue; // 범위벗어남
+            if(dist[nxt] >= 0) continue; // 방문 이력 존재
+            dist[nxt] = dist[cur] + 1;
+            Q.push(nxt);
         }
     }
+    cout << dist[k]; 
 }
