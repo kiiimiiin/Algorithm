2dcce3bfa524210d1b7b0b167c74a15b244d1a1e
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2023-01-31 21:44:57 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2544 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -2,26 +2,31 @@
 using namespace std;
 #define X first
 #define Y second
+
 int dist[100002];
-int n,k;
+int n, k;
+
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0);
+    
     cin >> n >> k;
-    fill(dist, dist+100001, -1); //방문 이력 없음
+    fill(dist, dist + 100001, -1); // 방문이력 없음
+    
     queue<int> Q;
     dist[n] = 0;
-    Q.push(n); // 수진 시작점
-    while(dist[k] == -1) // 동생 위치를 차지하기 전까지 BFS
+    Q.push(n);
+
+    while(dist[k] == -1)
     {
         int cur = Q.front(); Q.pop();
-        for(int nxt : {cur-1, cur+1, 2*cur}) // range-based for
+        for( int nxt : {cur + 1, cur - 1, cur*2})
         {
-            if(nxt < 0 || nxt > 100000) continue; // 범위벗어남
+            if(nxt < 0 || nxt > 100000) continue; // 영역 밖
             if(dist[nxt] >= 0) continue; // 방문 이력 존재
             dist[nxt] = dist[cur] + 1;
             Q.push(nxt);
         }
     }
-    cout << dist[k]; 
+    cout << dist[k] ;
 }
