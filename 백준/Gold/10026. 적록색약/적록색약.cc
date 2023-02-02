63fadbfd39918a78eb6f8dd090a0fc47039c291c
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2023-02-02 15:47:54 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -1,89 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define X first
-#define Y second 
-// RGB로 그림구분
-// 적록색약 RG , B | 일반인 R,G,B 그림 영역 구분
-// B를 먼저 돌려서 B 영역수  -> R, G를 나눠서 돌림 -> R,G를 합쳐서 돌림 
-string board[102];
+#define Y second
+char board[101][101];
+bool vis[101][101];
 int n;
 int dx[4] = { 1, 0, -1, 0};
 int dy[4] = { 0, 1, 0, -1};
-int vis[102][102];
-queue<pair<int,int>> color;
 
-int colorNumBfs(char colorName)
+void bfs(int i, int j)
 {
-    int Num = 0;
-    for(int i = 0 ; i < n; i++) // 색에 대한 bfs
+    queue<pair<int, int>> Q;
+    vis[i][j] = 1;
+    Q.push({i,j});
+    while(!Q.empty())
     {
-        for(int j = 0; j < n; j++)
+        auto cur = Q.front(); Q.pop();
+        for(int dir = 0; dir < 4 ; dir++)
         {
-            if(board[i][j] == colorName && vis[i][j] == 0)
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+            if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+            if(vis[nx][ny] == 1 || board[nx][ny] != board[cur.X][cur.Y]) continue; //같은 색이 아님
+            vis[nx][ny] = 1;
+            Q.push({nx,ny});            
+        }
+    }
+}
+
+int area()
+{
+    int cnt = 0;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0 ; j < n; j++)
+        {
+            if(!vis[i][j])
             {
-                Num++;
-                vis[i][j] = 1;
-                color.push({i,j});
-                while(!color.empty())
-                {
-                    auto cur = color.front(); color.pop();
-                    for(int dir = 0; dir < 4 ; dir++)
-                    {
-                        int nx = cur.X + dx[dir];
-                        int ny = cur.Y + dy[dir];
-                        if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-                        if(board[nx][ny] != colorName || vis[nx][ny] == 1) continue;
-                        vis[nx][ny] = 1;
-                        color.push({nx,ny}); 
-                    }
-                }
+                cnt++;
+                bfs(i,j); // 방문하지 않은 영역에 대해 bfs 
             }
         }
     }
-    return Num;
+    return cnt;
 }
-int redgreenBfs()
-{
-    int Num = 0;
-    for(int i = 0 ; i < n; i++) 
-    {
-        for(int j = 0; j < n; j++)
-        {
-            if((board[i][j] == 'R'|| board[i][j] == 'G') && vis[i][j] == 0)
-            {
-                Num++;
-                vis[i][j] = 1;
-                color.push({i,j});
-                while(!color.empty())
-                {
-                    auto cur = color.front(); color.pop();
-                    for(int dir = 0; dir < 4 ; dir++)
-                    {
-                        int nx = cur.X + dx[dir];
-                        int ny = cur.Y + dy[dir];
-                        if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-                        if(board[nx][ny] == 'B' || vis[nx][ny] == 1) continue;
-                        vis[nx][ny] = 1;
-                        color.push({nx,ny}); 
-                    }
-                }
-            }
-        }
-    }
-    return Num;
-}
-
-int main()
-{
+int main() {
     ios::sync_with_stdio(0), cin.tie(0);
-    
     cin >> n;
-    for(int i = 0 ; i < n; i++) cin >> board[i];
-    int redNum = colorNumBfs('R');
-    int greenNum = colorNumBfs('G');
-    int blueNum = colorNumBfs('B');
-    for(int i = 0 ; i < 102 ; i++) fill(vis[i], vis[i] + 102, 0);
-    int redgreenNum = redgreenBfs();
+    for(int i = 0 ; i < n; i++)
+        for(int j = 0; j <n; j++)
+            cin >> board[i][j];
+    
+    int not_g = area(); // 일반인에 대한 그림 영역 수 
+    
+    for(int i = 0; i < n ; i++) fill(vis[i], vis[i] + n, 0); //방문영역 초기화
+    
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            if(board[i][j] == 'G')
+                board[i][j] = 'R'; // R,G를 같게함
+        }
+    }
 
-    cout << blueNum + redNum + greenNum << ' ' << redgreenNum + blueNum ;
+    int is_g = area(); // 적록색약에 대한 그림 영역 수 
+    cout << not_g << " " << is_g;
 }
