2d1231ba2d5c5f2ea417ef41108e028d85999aae
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2023-02-08 22:45:38 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,29 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-int cnt, n; 
 
-void func(int tot) // 합이 n이 될 때 cnt올림
-{
-    if(tot >= n)
-    {
-        if(tot == n) cnt++;
-        return ;
-    }
-    func(tot+1);
-    func(tot+2);
-    func(tot+3);
-}
-
+int d[20];
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0);
+    d[1] = 1; d[2] = 2; d[3] = 4;
+    for(int i = 4; i < 11; i++)
+        d[i] = d[i-1] + d[i-2] + d[i-3];
     int t;
     cin >> t;
     while(t--)
     {
+        int n;
         cin >> n;
-        func(0);
-        cout << cnt << '\n';
-        cnt = 0;
+        cout << d[n] << '\n';
     }
 }
