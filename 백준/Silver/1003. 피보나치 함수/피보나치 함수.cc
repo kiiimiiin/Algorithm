38f8652b93c2d7e3dbdebfdfcd90f6ddd232bbfc
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2023-02-09 11:30:34 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int dp[45][2]; // 정수에 대한 fibo[0], fibo[1] 함수의 호출횟수
+int n,t;
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> t;
+    dp[0][0] = 1; dp[1][0] = 0;
+    dp[0][1] = 0; dp[1][1] = 1;
+    
+    for(int i = 2 ; i <= 40; i++)
+    {
+        dp[i][0] = dp[i-1][0] + dp[i-2][0];
+        dp[i][1] = dp[i-1][1] + dp[i-2][1];        
+    }
+    while(t--)
+    {
+        cin >> n;
+        cout << dp[n][0] << ' ' << dp[n][1] << '\n';
+    }    
+}
