9c1fa7f927409fa42b5d7774ea76d993b8c8e3af
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2023-02-17 11:52:28 +0900

    [Silver III] Title: N과 M (5), Time: 40 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+vector<int> nArray;
+int mArray[10];
+bool isused[10002]; 
+void backTracking(int idx) // 현재 수열의 길이, idx에 수를 놓을 차례
+{
+    if(idx == m)
+    {
+        for(int i = 0; i < m; i++)
+            cout << mArray[i] << ' ';
+        cout << '\n';
+        return ;
+    }
+    
+    for( int i : nArray)
+    {
+        if(isused[i]) continue;
+        mArray[idx] = i;
+        isused[i] = true;
+        backTracking(idx + 1);
+        isused[i] = false;   
+    }
+}
+int main()
+{
+    cin >> n >> m;
+    for(int i = 0; i < n; i++)
+    {
+        int init;
+        cin >> init;
+        nArray.push_back(init);
+    }
+    sort(nArray.begin(), nArray.end());
+    backTracking(0);
+}
