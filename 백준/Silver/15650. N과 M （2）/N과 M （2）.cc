b6a94bc17d3984aeeb0b44863f2dba0600680e29
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2023-02-17 11:15:30 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,18 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int N, M;
-vector<int> a;
-
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> N >> M;
-  // M의 개수만큼 0을 넣어줌
-  for(int i = 0; i < N; ++i)  a.push_back(i < M ? 0 : 1);
-  do{
-    for(int i = 0; i < N; ++i)
-      if(a[i] == 0) cout << i+1 << ' ';
-    cout << '\n';
-  }while(next_permutation(a.begin(), a.end()));
+int n,m ;
+int arr[10];
+int main()
+{
+    cin >> n >> m;
+    fill(arr + m, arr + n, 1); // m개를 택함 
+    do{
+        for(int i = 0; i < n ; i++)
+        {
+            if(arr[i] == 0)
+            cout << i + 1 << ' ';
+        }
+        cout << '\n';
+    }while(next_permutation(arr, arr+n));
 }
