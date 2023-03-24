e7c1d0cf968f5b64223e1e77874b9c2c9c12b959
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2023-03-25 00:06:08 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,32 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define X first
-#define Y second
-
+int n, k; // 수빈이의 위치 , 동생의 위치 
 int dist[100002];
-int n, k;
-
+queue<int> Q;
 int main()
 {
-    ios::sync_with_stdio(0), cin.tie(0);
-    
-    cin >> n >> k;
-    fill(dist, dist + 100001, -1); // 방문이력 없음
-    
-    queue<int> Q;
-    dist[n] = 0;
-    Q.push(n);
+	cin >> n >> k;
+	fill(dist, dist + 100001, -1); // 방문이력 없음
+	dist[n] = 0;
+	Q.push(n); // 수빈 시작점
 
-    while(dist[k] == -1)
-    {
-        int cur = Q.front(); Q.pop();
-        for( int nxt : {cur + 1, cur - 1, cur*2})
-        {
-            if(nxt < 0 || nxt > 100000) continue; // 영역 밖
-            if(dist[nxt] >= 0) continue; // 방문 이력 존재
-            dist[nxt] = dist[cur] + 1;
-            Q.push(nxt);
-        }
-    }
-    cout << dist[k] ;
+	while(dist[k] == -1) // 동생이 있는 지점에 방문이력이 없는 동안 bfs
+	{
+		auto cur = Q.front(); Q.pop();
+		for( auto nx : {cur - 1, cur + 1 , 2*cur})
+		{
+			if(nx < 0 || nx >= 100001 || dist[nx] >= 0) continue; //범위 벗어나거나 방문이력 존재
+			dist[nx] = dist[cur] + 1;
+			Q.push(nx);
+		}
+	}
+	cout << dist[k];
 }
