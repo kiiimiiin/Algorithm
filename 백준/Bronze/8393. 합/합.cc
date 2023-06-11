3f699eb0c7c24340ead8bc7f7859eeaa7088075b
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2023-06-11 15:43:45 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n, sum = 0;
+    cin >> n;
+    for(int i = 1 ; i <= n ; i++)
+        sum += i;
+    cout << sum;
+}
