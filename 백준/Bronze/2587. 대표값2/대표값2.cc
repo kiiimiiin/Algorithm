e4e09de708d25f5f1d0122594bab2a65f68bff61
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2023-06-12 16:25:09 +0900

    [Bronze II] Title: 대표값2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.cc
+++ 백준/Bronze/2587. 대표값2/대표값2.cc
@@ -1,15 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int arr[7], sum = 0;
-
-int main() {
-    ios::sync_with_stdio(0), cin.tie(0);
-
-    for(int i = 0 ; i < 5 ; i++) cin >> arr[i];
-    for(int i = 0 ; i < 5 ; i++) sum += arr[i];
-    cout << sum / 5 << '\n' ;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     
-    sort(arr, arr+5);
-    cout << arr[2];
+    int num[5];
+    int sum = 0;
+    for(int i = 0 ; i < 5; i++)
+    {
+        cin >> num[i];
+        sum += num[i];
+    }
+    sort(num, num + 5);
+    cout << sum / 5 << '\n' << num[2] ;
 }
