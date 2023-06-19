660fedf9d6390fd27fe19dada76e06cf4c4cde00
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2023-06-19 20:22:45 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.cc
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -1,24 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+int counts[10];
 
-int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  
-  int N, a[10] = {}, ans = 0;
-  cin >> N;
-  
-  // 자리수 추출
-  while(N){
-    a[N%10]++;
-    N /= 10;
-  }
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    int n;
+    cin >> n;
+    do{
+        int one_digit = n % 10;
+        counts[one_digit]++;
+        if(one_digit == 9) counts[6]++;
+    }while(n /= 10);
 
-  for(int i = 0; i < 10; i++){
-    if(i == 6 || i == 9) continue;
-    ans = max(ans, a[i]);
-  }
-  // (a[6]+a[9])/2를 올림한 값이 6, 9에 대한 필요한 세트의 수이므로 (a[6]+a[9]+1)/2을 계산
-  ans = max(ans, (a[6]+a[9]+1)/2);
-  cout << ans;
+    if(counts[6] % 2 == 0) counts[6] = counts[6] / 2 ; // 짝수 
+    else counts[6] = counts[6] / 2 + 1;
+    int* max = max_element(counts, counts+9);
+    cout << *max;
 }
