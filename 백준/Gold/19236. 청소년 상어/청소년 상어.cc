179c472ad569d4bf0f287bbabae558fa5eb285b2
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-03-28 17:43:36 +0900

    [Gold II] Title: 청소년 상어, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/19236. 청소년 상어/청소년 상어.cc
+++ 백준/Gold/19236. 청소년 상어/청소년 상어.cc
@@ -1,119 +1,121 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
-
 using namespace std;
 
 struct Fish {
-    int x,y;
-    int d;
-    bool is_lived;
+	int x, y;
+	int dir;
+	bool isLive;
 };
+int dx[8] = { -1,-1,0,1,1,1,0,-1 };
+int dy[8] = { 0, -1, -1,-1,0,1,1,1 };
+int mx = -0x7f7f7f7f;
+Fish fishes[17]; // 번호에 해당하는 물고기정보, 0은 dummy
+int board[5][5]; // 물고기 번호 보드
 
-int board[4][4]; // 물고기 번호 board
-Fish fishes[20]; // 물고기 번호에 대한 물고기 info
 
-int dx[8] = { -1, -1, 0, 1, 1, 1, 0, -1 };
-int dy[8] = { 0, -1, -1, -1, 0, 1, 1, 1 };
-int mx = -0x7f7f7f7f; 
-
-void Copy(int desc[4][4], int src[4][4], Fish desc2[20], Fish src2[20]){
-    for(int i = 0 ; i < 4; i++)
-        for(int j = 0 ; j < 4 ; j++)
-            desc[i][j] = src[i][j];
-    
-    for(int i = 0 ; i < 20; i++) desc2[i] = src2[i];
+bool OOB(int x, int y) {
+	return (x < 0 || x >= 4 || y < 0 || y >= 4);
 }
 
-void MoveFish(){
-    for(int i = 1; i <= 16; i++){
-        if(!fishes[i].is_lived) continue;
-        Fish f = fishes[i];
-        
-        int d = f.d;
-        int nx = f.x + dx[d];
-        int ny = f.y + dy[d];
-        while((nx < 0 || nx >= 4 || ny < 0 || ny >= 4) || board[nx][ny] == -1){ 
-            d = ( d + 1 ) % 8;
-            nx = f.x + dx[d];
-            ny = f.y + dy[d];
-        }
-    
-        fishes[i].d = d; 
+void MoveFish() {
 
-        // 물고기 존재 혹은 빈칸 swap
-        board[f.x][f.y] = board[nx][ny];
-        fishes[board[nx][ny]].x = f.x;
-        fishes[board[nx][ny]].y = f.y;
-        board[nx][ny] = i;
-        fishes[i].x = nx;
-        fishes[i].y = ny;
-    }
+	for (int num = 1; num <= 16; num++) {
+		if (!fishes[num].isLive) continue;
+
+		int nx, ny; // 이동할 수 있는 칸 찾음.
+		int cnt = 8;
+		while (cnt--) {
+			nx = fishes[num].x + dx[fishes[num].dir];
+			ny = fishes[num].y + dy[fishes[num].dir];
+			if (OOB(nx, ny) || board[nx][ny] == -2) {
+				fishes[num].dir = (fishes[num].dir + 1) % 8;
+				continue;
+			}
+			else
+				break; 
+		} // 타겟 위치 방향 설정
+
+		if (cnt >= 0) {// 물고기 존재 : 위치변환
+			int targetNum = board[nx][ny];
+
+			board[nx][ny] = num;
+			board[fishes[num].x][fishes[num].y] = targetNum; // 보드 정보 수정
+
+			fishes[targetNum].x = fishes[num].x;
+			fishes[targetNum].y = fishes[num].y;
+
+			fishes[num].x = nx;
+			fishes[num].y = ny; // 번호별 물고기 정보 수정
+			
+		}
+	}
 }
 
-void DFS(int x, int y, int sum, int d){ 
-    // parm : 상어 x,y / 먹은 물고기 번호 합 / 상어 방향
-    // 상어가 x,y에 대한 물고기를 먹었을때의 합을 구함
+void Copy(int desc[5][5], int src[5][5], Fish desc2[17], Fish src2[17]) {
+	for (int x = 0; x < 4; x++) {
+		for (int y = 0; y < 4; y++) {
+			desc[x][y] = src[x][y]; 
+		}
+	}
 
-    mx = max(mx , sum); // 1.  맥스값 구함 
-    int cboard[4][4]; 
-    Fish cfishes[20];
-    Copy(cboard, board, cfishes, fishes);
-    
-    MoveFish(); // 2. 물고기 이동
-    
-    for(int i = 1; i <= 3; i++){ // 3. 상어 이동
-        int nx = x + i * dx[d];
-        int ny = y + i * dy[d];
-        if(nx < 0 || nx >= 4 || ny < 0 || ny >= 4) continue;
-        if(board[nx][ny] == 0) continue;  // 빈칸
-        
-        // eat fish
-        int n = board[nx][ny];
-        int nd = fishes[n].d;
-        fishes[n].is_lived = false;
-        board[nx][ny] = -1;
-        board[x][y] = 0;
-        
-        DFS(nx, ny, sum + n, nd);
-        
-        board[nx][ny] = n;
-        fishes[n].is_lived = true; 
-        board[x][y] = -1; // 원복
-    }
-    
-    Copy(board, cboard, fishes, cfishes); // 보드 원복 
+	for (int num = 1; num <= 16; num++) 
+		desc2[num] = src2[num];
+}
 
+void dfs(int sum, int sx, int sy, int sd) {
+	mx = max(mx, sum);
+
+	int cBoard[5][5]; Fish cFishes[17]; 
+	Copy(cBoard, board, cFishes, fishes);
+	MoveFish();
+	
+
+	for (int s = 1; s <= 3; s++) {
+		int snx = sx + s * dx[sd];
+		int sny = sy + s * dy[sd];
+		if (OOB(snx, sny)) break;
+		if (board[snx][sny] == 0) continue;
+		
+		
+		int targetNumber = board[snx][sny];
+		int snd = fishes[targetNumber].dir;
+		fishes[targetNumber].isLive = false;
+		board[sx][sy] = 0;
+		board[snx][sny] = -2;
+		dfs(sum + targetNumber, snx, sny, snd);
+
+		
+		fishes[targetNumber].isLive = true;
+		board[sx][sy] = -2;
+		board[snx][sny] = targetNumber;
+	}
+
+	Copy(board, cBoard, fishes, cFishes);
 }
 
 
+int main(void) {
 
-// DFS 과정:
-// 물고기를 이동한 경우에 대해 상어가 이동할 수 있는 모든 경우를 본다.
-// 각 경우가 끝나면 해당 물고기가 이동한 경우는 끝났으므로 보드 원복
-// 이전 경우에서 상어가 이동할 다음 경우로 넘어간다.
+	for (int x = 0; x < 4 ; x++) {
+		for (int y = 0; y < 4; y++) {
+			int num, dir; 
+			cin >> num >> dir;
+			fishes[num] = { x, y, dir-1, true };
+			board[x][y] = num;
+		}
+	}
 
-int main(void){
-    ios::sync_with_stdio(0), cin.tie(0);
+	// (0,0) 먹기
+	int sx, sy, sd, sum;
+	sx = 0; sy = 0; sum = board[0][0];
+	sd = fishes[board[0][0]].dir; 
+	fishes[board[0][0]].isLive = false;
+	board[0][0] = -2;
+	
+	dfs(sum, sx, sy, sd);
+	
+	cout << mx;
 
-    for(int i = 0; i < 4; i++){
-        for(int j = 0 ; j < 4; j++){
-            int n, d;
-            cin >> n >> d;
-            board[i][j] = n;
-            fishes[n].x = i;
-            fishes[n].y = j;
-            fishes[n].d = d-1;
-            fishes[n].is_lived = true;
-        }
-    }
-    
-    // (0, 0) 물고기 섭취
-    int n = board[0][0];
-    int sd = fishes[n].d;
-    fishes[board[0][0]].is_lived = false;
-    board[0][0] = -1; // 상어 존재 : -1 
-    
-    DFS(0, 0, n, sd);
-    
-    cout << mx;
 }
