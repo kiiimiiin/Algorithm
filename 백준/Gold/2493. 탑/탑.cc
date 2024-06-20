36832ab48afea3c1cf4d5a7947ca4163040d597e
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-06-21 00:50:34 +0900

    [Gold V] Title: 탑, Time: 88 ms, Memory: 6300 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -1,25 +1,33 @@
 #include <iostream>
-#include <utility>
 #include <stack>
 using namespace std;
- 
+
 int main(void){
     ios::sync_with_stdio(0), cin.tie(0);
     int n; 
+    stack<pair<int,int>> s;
+    s.push({100000001, 0}); // 벗어난 레이저를 처리하기 위함
+    
     cin >> n;
     
-    stack<pair<int,int>> S;
-    S.push({100000001, 0}); 
-    
     for(int i = 1 ; i <= n; i++){
-        int height; // 현재 인덱스 탑 높이
+        int height;
         cin >> height; 
         
-        while(S.top().first < height){ // 앞에 탑들과 높이비교
-            S.pop(); 
+        while( s.top().first < height){ // 앞에 탑이 낮음
+            s.pop();
         }
         
-        cout << S.top().second << ' '; // 도달한 탑의 인덱스
-        S.push({height, i}); // 현재 탑 추가
+        cout << s.top().second << ' '; // 앞에 탑이 높거나 같아 수신함 
+        s.push({height, i});
     }
 }
+
+/*
+    스택으로 탑을 하나 씩 처리 
+    앞에 있는 탑이 높이가 낮다 -> 무시
+    앞에 있는 탑이 높이가 높거나 같다 -> 수신 (수신 인덱스)
+    
+    높이와 수신 인덱스 필요
+    
+*/
