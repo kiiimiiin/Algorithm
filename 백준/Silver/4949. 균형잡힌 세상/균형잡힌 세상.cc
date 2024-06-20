217ce8e6795893ed80bf6e874105572d2dc41305
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-06-20 14:58:10 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -1,53 +1,46 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <stack>
+#include <string>
 using namespace std;
 
-int main()
-{
+int main(void){
     ios::sync_with_stdio(0), cin.tie(0);
     
-    stack<char> S;
     string s;
-    string yesOrNo;
-
-    while(1)
-    {  
-        yesOrNo = "yes";
+    
+    while(1){
+        stack<char> S; 
+        string flag = "yes";
+        
         getline(cin, s);
         if(s == ".") break;
-        for(auto c : s)
-        {
-            if(c == '[' || c== '(')
+        
+        for(auto c : s){
+            if(c == '(' || c == '['){ // 여는 괄호 -> 스택 추가
                 S.push(c);
-            else if( c == ']' || c == ')')
-                {
-                    if(S.empty()) 
-                    {
-                        yesOrNo = "no";
-                        break;
-                    }
-                    else if( c == ']')
-                    {
-                        if(S.top() != '[')
-                        {
-                            yesOrNo = "no";
-                            break;
-                        }
-                        else S.pop();
-                    }
-                    else if( c == ')')
-                    {
-                        if(S.top() != '(')
-                        {
-                            yesOrNo = "no";
-                            break;
-                        }
-                        else S.pop();
-                    }
-                }
+            }
+            else if(c == ')' || c == ']'){ // 닫힌 괄호
+                   if(S.empty()){ // 앞이 비어있음 -> fail
+                        flag = "no";
+                        break;   
+                   }
+                   
+                   if(S.top() == '(' && c == ')' ||
+                    S.top() == '[' && c == ']' ){
+                        S.pop(); // 짝을 찾으면 pop   
+                   }
+                   else{ // 짝과 다름 -> fail
+                       flag = "no";
+                       break;   
+                   }
+            }
         }
-        if(!S.empty())
-            yesOrNo = "no";
-        std::cout << yesOrNo << '\n';
-        while(!S.empty()) S.pop();
-    }                
+        
+        
+        if(!S.empty()) // 스택에 열린괄호 존재 -> fail 
+            flag = "no";
+        
+        cout << flag << '\n';
+    }
+    
 }
