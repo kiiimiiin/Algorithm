3ac1ce9afda91337334e15dca222649e7d3aef2d
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-06-22 18:41:29 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -1,65 +1,42 @@
-#include <string>
 #include <iostream>
-#include <utility>
-#include <deque> 
+#include<algorithm>
+#include <functional>         // greater 사용 위해 필요  
 #include <vector>
-#define W first
-#define T second
-
+#include<queue>
 using namespace std;
-
-int getOnTruckWeight(deque<pair<int,int>>& bridge){
-    int sum = 0;
-    for(auto t : bridge){
-        sum += t.W;
-    }
-    return sum;
-}
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
     int answer = 0;
-    int truck_num = truck_weights.size(); 
-    
-    vector<int> passed_trucks;
-    deque<pair<int,int>> trucks_on_bridge; // 트럭무게, 빠질시간
-    
-    int pop_time = -1;
-    int waiting_idx = 0;
-    
-    while(passed_trucks.size() != truck_num){
-        
-        answer++;
-        
-       
-        
-        int waiting_truck = truck_weights[waiting_idx];
-        
-        if(!trucks_on_bridge.empty() && trucks_on_bridge.front().T == answer){
-            passed_trucks.push_back(trucks_on_bridge.front().W);
-            trucks_on_bridge.pop_front();
+    int count = 0;
+    int Time = 0; 
+    int Truck_weight = 0;
+    queue<pair<int, int>> truck_move;
+
+    while (true)
+    {
+        if (weight >= Truck_weight + truck_weights.at(count))
+        {
+            truck_move.push(make_pair(truck_weights.at(count), bridge_length + 1 + Time));
+            Truck_weight += truck_weights.at(count);
+            count++;
         }
-        
-        if(trucks_on_bridge.size() < bridge_length && 
-           (getOnTruckWeight(trucks_on_bridge) + waiting_truck) <= weight){
-            // 다리 위 트럭 수가 다리 길이보다 작고
-            // 다리 위 트럭 무게와 건널 대기 트럭 무게합이 한도무게와 작거나 같을 때 트럭이 건넘
-            
-            pop_time = answer + bridge_length; 
-            
-            trucks_on_bridge.push_back({waiting_truck, pop_time});
-            
-            waiting_idx++;
-            
-            cout << "(" << waiting_truck << "," << pop_time << ")\n" ; 
-            
+
+        if (count >= truck_weights.size())
+        {
+            answer = truck_move.back().second;
+            break;
         }
-        
-        
-        
-        
-        
+        else
+        {
+            Time++;
+            if (truck_move.front().second == Time+1)
+            {
+                Truck_weight -= truck_move.front().first;
+                truck_move.pop();
+            }
+        }
+
     }
-    
+
     return answer;
 }
-
