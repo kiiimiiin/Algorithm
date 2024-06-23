bea7635e50abaed22f56308d3bbb85c4b92f0eb9
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-06-23 18:41:45 +0900

    [level 2] Title: 주식가격, Time: 15.51 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -7,18 +7,17 @@ using namespace std;
 
 vector<int> solution(vector<int> prices) {
     vector<int> answer(prices.size());
-    stack<pair<int,int>> S; // {주식가격, 인덱스}
-    S.push({-1, -1});
+    stack<int> S; // 인덱스
     
     for(int i = 0; i < prices.size(); i++){
         
-        while(S.top().first > prices[i]){
-            answer[S.top().second] = i - S.top().second;
+        while(!S.empty() && prices[S.top()] > prices[i]){
+            answer[S.top()] = i - S.top();
             S.pop();
         }
         
         answer[i] = ( prices.size() - 1) - i; 
-        S.push({prices[i], i});
+        S.push(i);
         
     }
     
