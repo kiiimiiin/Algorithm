c8f7cff498861a56023e1cbb33fc67e41beb79f5
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-07-07 22:05:37 +0900

    [Gold IV] Title: N-Queen, Time: 5136 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,80 +1,43 @@
 #include <iostream>
-#define X first
-#define Y second
+
 using namespace std;
-int n, ans;
-int board[17][17];
-int dx[8] = { 1, 1, 1, 0, 0, -1, -1, -1 };
-int dy[8] = { -1, 0, 1, -1, 1, -1, 0, 1 };
-    
-bool OOB(int x, int y){
-    return (x < 0 || x >= n || y < 0 || y >= n);
-}
-    
-void test(){
-    for(int i = 0 ; i < n; i++){
-        for(int j = 0 ; j < n; j++){
-            cout << board[i][j] << ' ';
-        }
-        cout << '\n';
-    }
-    cout << '\n';
-}
 
-void mark(int curX, int curY){
-    board[curX][curY]++; // 퀸
-    for(int dir = 0 ; dir < 8; dir++){
-        int nx = curX + dx[dir];
-        int ny = curY + dy[dir];
-        while(!OOB(nx,ny)){
-            board[nx][ny]++;; // 공격불가 지역
-            nx += dx[dir];
-            ny += dy[dir];
-        }
+int N;
+int col[15];
+int result = 0;
+
+bool promising(int i)
+{
+    for(int j=0;j<i;j++)
+    {
+        // 새로운 퀸과 기존의 퀸이 같은 행에 있거나 대각선에 있을 경우
+        if(col[j] == col[i] || abs(col[i]-col[j]) == (i-j))
+            return false;
     }
-    
-    //test();
+    return true;
 }
-
-void unmark(int curX, int curY){
-    board[curX][curY]--; // 퀸
-    for(int dir = 0 ; dir < 8; dir++){
-        int nx = curX + dx[dir];
-        int ny = curY + dy[dir];
-        while(!OOB(nx,ny)){
-            board[nx][ny]--; // 공격불가 지역
-            nx += dx[dir];
-            ny += dy[dir];
-        }
-    }
-    
-    //test();
-}
-
-void dfs(int i){ // i번째 행에 퀸을 둘 차례
-
-    if(i == n ){
-        ans++;
-        return ;
-    }
-    
-    for(int j = 0 ; j < n; j++){
-        if(board[i][j] == 0){
-            mark(i,j);
-            dfs(i + 1);
-            unmark(i,j);
+void N_Queen(int i)
+{
+    if(i == N)
+        result += 1;
+    else
+    {
+        for(int j=0;j<N;j++)
+        {
+            col[i] = j;
+            if(promising(i))
+                N_Queen(i+1);
         }
     }
 }
 
-int main(void){
-    ios::sync_with_stdio(0), cin.tie(0);
-    cin >> n;
-    dfs(0);
-    cout << ans;
+int main()
+{
+    cin>>N;
+
+    N_Queen(0);
+
+    cout<<result<<endl;
+
+    return 0;
 }
-
-
-/* 
-    N*N에 N개의 퀸을 놓으려면 모든 행에 퀸이 있어야 한다.
-*/
