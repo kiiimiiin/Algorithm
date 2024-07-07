1b2bcb499342384af0ecedae1b9cfc42ed00940e
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-07-07 16:55:02 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,33 +1,34 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 int n, m;
 int arr[10];
-int isused[10];
+bool isUsed[10];
 
-void func(int k) // k개 택함
-{
-    if(k == m) // 리스트에 m개 존재
-    {
-        for(int i = 0; i < m; i++)
+
+void dfs(int k){ // k번째 indexing
+    if(k == m){
+        for(int i = 0 ; i < m; i++){
             cout << arr[i] << ' ';
+        }
         cout << '\n';
         return ;
-    } // base condition
-
-    for(int i = 1 ; i <= n ; i++) // 리스트에 원소를 집어넣음
-    {
-        if(!isused[i]) // 사용안한 원소 , 1
-        {
-            arr[k] = i; // 1 2
-            isused[i] = true;
-            func(k+1); // k+1개 택함 , 1 2 3 1 2 4 
-            isused[i] = false; // 1 2 의 경우 모두 확인 
+    }    
+    
+    for(int i = 1; i <= n; i++){
+        if(!isUsed[i]){
+            arr[k] = i;
+            isUsed[i] = true; 
+            dfs(k + 1);
+            isUsed[i] = false;
         }
     }
 }
-int main()
-{
+
+
+
+
+int main(void){
     ios::sync_with_stdio(0), cin.tie(0);
     cin >> n >> m;
-    func(0);
+    dfs(0);
 }
