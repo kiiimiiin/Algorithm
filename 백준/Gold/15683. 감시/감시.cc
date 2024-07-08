029b656ce1cb201261bac2804ffc9c70193c4972
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-07-08 13:56:05 +0900

    [Gold III] Title: 감시, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.cc
+++ 백준/Gold/15683. 감시/감시.cc
@@ -1,100 +1,95 @@
 #include <bits/stdc++.h>
 using namespace std;
-// CCTV가 주어지고 CCTV에는 고유 방향이 존재
-// 각 방향에 대한 모든 경우를 확인 Backtraking
-// 복잡도 4^8 * 64 << 1억 -> ok
-int n , m, emptyMin = 68;
+int n, m;
+int mn = 0x7f7f7f7f;
 int board[10][10];
-int dirInfo[5] = {4, 2, 4, 4, 1};
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
-vector<tuple<int,int,int>> cctv; // cctv type , x, y 
+int dx[4] = { 1, 0, -1 ,0 };
+int dy[4] = { 0, 1, 0, -1 }; 
 
-void boardCopy(int desc[10][10], int src[10][10])
-{
-    for(int i = 0; i < n ; i++)
-        for(int j = 0 ; j < m ; j++)
-            desc[i][j] = src[i][j];
+vector <tuple<int,int,int>> cctvs; // cctv위치, 종류
+
+void copy(int dest[10][10], int src[10][10]){
+    for(int i = 0 ; i < n; i++)
+        for(int j = 0; j < m; j++)
+            dest[i][j] = src[i][j];
 }
 
-void cctvDetect(int dir, int cctvX, int cctvY)
-{ //dir 한 방향 감시
-    dir %= 4 ;
-    int detectedX = cctvX;
-    int detectedY = cctvY;
-    while(1)
-    {
-        detectedX += dx[dir];
-        detectedY += dy[dir];
-        if(detectedX < 0 || detectedX >= n || detectedY < 0 || detectedY >= m) break;
-        if(board[detectedX][detectedY] == 6) break; // 벽이거나 외부로 나가면 종료
-        if(board[detectedX][detectedY] != 0) continue;
-        board[detectedX][detectedY] = 7; // 감시표시
-    }
-}
-void backTracking(int cctvNum) // cctv 확인 수
-{
-    if(cctvNum == cctv.size())
-    {   
-        int emptyNum = 0;
-        for(int i = 0; i < n; i++)
-            for(int j = 0 ; j < m; j++)
-                if(board[i][j] == 0) emptyNum++;
-        emptyMin = min(emptyMin, emptyNum);
-        return ;
-    } // 경우마다 빈공간 확인 ( cctv 확인 수가 cctv 갯수랑 같아질 때가 종료조건)
-
-    int cctvType, cctvX, cctvY;
-    tie(cctvType, cctvX, cctvY) = cctv[cctvNum];
-    for(int dir = 0 ; dir < dirInfo[cctvType - 1] ; dir++)
-    {
-        int tmp[10][10];
-        boardCopy(tmp , board);
-        if(cctvType == 1)
-        {
-            cctvDetect(dir, cctvX, cctvY);
-        }
-        if(cctvType == 2)
-        {
-            cctvDetect(dir, cctvX, cctvY);
-            cctvDetect(dir + 2, cctvX, cctvY);
-        }
-        if(cctvType == 3)
-        {
-            cctvDetect(dir, cctvX, cctvY);
-            cctvDetect(dir + 1, cctvX, cctvY);
-        }
-        if(cctvType == 4)
-        {
-            cctvDetect(dir, cctvX, cctvY);
-            cctvDetect(dir + 1, cctvX, cctvY);
-            cctvDetect(dir + 2, cctvX, cctvY);
-        }
-        if(cctvType == 5)
-        {
-            cctvDetect(dir, cctvX, cctvY);
-            cctvDetect(dir + 1, cctvX, cctvY);
-            cctvDetect(dir + 2, cctvX, cctvY);
-            cctvDetect(dir + 3, cctvX, cctvY);
-        }
-        backTracking(cctvNum + 1);
-        boardCopy(board, tmp);
+void detect(int cctv_x, int cctv_y, int dir){
+    dir = dir % 4;
+    
+    int nx = cctv_x; int ny = cctv_y;
+    
+    while(1){
+        nx += dx[dir]; ny += dy[dir]; 
+        if(nx < 0 || nx >= n || ny < 0 || ny >= m ) break;
+        if(board[nx][ny] == 6) break;
+        if(board[nx][ny] >= 1 && board[nx][ny] <= 5) continue;
+        board[nx][ny] = 7;
     }
     
-}
+} 
 
-int main()
-{
+void dfs(int idx){
+    if(idx == cctvs.size()){
+        int area = 0;
+        for(int i = 0 ; i < n; i++)
+            for(int j = 0; j < m; j++)
+                if(board[i][j] == 0) area++;
+        
+        mn = min(mn, area);     
+        return ;
+    }
+    
+    
+    int backup[10][10];
+    int cctv_x, cctv_y, cctv_type;
+    tie(cctv_x, cctv_y, cctv_type) = cctvs[idx];
+    
+    for(int dir = 0 ; dir < 4; dir++){
+        
+        copy(backup, board);
+        
+        if(cctv_type == 1){
+            detect(cctv_x, cctv_y, dir);
+        }
+        else if(cctv_type == 2){
+            detect(cctv_x, cctv_y, dir);
+            detect(cctv_x, cctv_y, dir + 2);
+        }
+        else if(cctv_type == 3){
+            detect(cctv_x, cctv_y, dir);
+            detect(cctv_x, cctv_y, dir + 1);
+        }
+        else if(cctv_type == 4){
+            detect(cctv_x, cctv_y, dir);
+            detect(cctv_x, cctv_y, dir + 1);
+            detect(cctv_x, cctv_y, dir + 2);
+        }
+        else if(cctv_type == 5){
+            detect(cctv_x, cctv_y, dir);
+            detect(cctv_x, cctv_y, dir + 1);
+            detect(cctv_x, cctv_y, dir + 2);
+            detect(cctv_x, cctv_y, dir + 3);
+        }
+        
+        dfs(idx + 1);
+        
+        copy(board, backup);
+        
+    }
+}
+int main(void){
+    ios::sync_with_stdio(0), cin.tie(0);
+    
     cin >> n >> m;
-    for(int i = 0 ; i < n ; i++)
-    {
-        for(int j = 0 ; j < m ; j++)
-        {
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
             cin >> board[i][j];
-            if(board[i][j] != 0 && board[i][j] != 6) // cctv정보 추출
-                cctv.push_back({board[i][j], i, j});
+            if(board[i][j] >= 1 && board[i][j] <= 5) cctvs.push_back({i,j, board[i][j]});
         }
     }
-    backTracking(0);
-    cout << emptyMin; 
+    
+    dfs(0); // 선택한 cctv수
+    
+    cout << mn; 
 }
