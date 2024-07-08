8e63e78aa7f4787a649cd7bd52c753f24d4e8930
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-07-08 11:13:06 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -1,28 +1,25 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 using namespace std;
- // 모든 후보군을 다 탐색하는 알고리즘 (백트래킹)
- int n, s, cnt;
- int arr[25]; 
+int n, s, ans;
+int arr[22];
 
-void backTracking(int t, int cur) // t는 현재 부분 수열의 총합, cur는 원소를 택한 횟수
-{
-    if(cur == n) // 원소가 끝까지 갔을 때 종료
-    {
-        if( t == s) cnt++; // 이때 합이 s와 같아야함
+void dfs(int k, int sum){
+    
+    if(k == n){
+        if(sum == s) ans++;
         return ;
     }
     
-    backTracking( t , cur + 1);
-    backTracking( t + arr[cur], cur + 1);
+    
+    dfs(k + 1, sum + arr[k]);
+    dfs(k + 1, sum);
 }
 
-int main()
-{
+int main(){
     ios::sync_with_stdio(0), cin.tie(0);
     cin >> n >> s;
-    for(int i = 0; i < n; i++) cin >> arr[i];
-
-    backTracking(0, 0); 
-    if(s == 0) cout << cnt - 1 ;
-    else cout << cnt ; // 크기가 양수인 부분수열이므로 공집합은 제외됨.
+    for(int i = 0 ;  i < n; i++) cin >> arr[i];
+    dfs(0, 0);
+    cout << (s == 0 ? ans -1 : ans);
 }
