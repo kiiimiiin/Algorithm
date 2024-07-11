359d009f15b7634ec4502c5327d044fb3d17b974
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-07-11 11:44:27 +0900

    [Gold III] Title: 아기 상어, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.cc
+++ 백준/Gold/16236. 아기 상어/아기 상어.cc
@@ -1,77 +1,101 @@
-#include <bits/stdc++.h>
-using namespace std; 
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <queue>
+#include <algorithm>
 #define X first
 #define Y second
-int dx[4] = { 1, 0, -1, 0 };
-int dy[4] = { 0, 1, 0, -1};
+using namespace std;
+int n, sx, sy, eaten;
+int ss = 2;
+int board[22][22];
 int dist[22][22];
-int board[22][22]; 
-int n;
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, -1, 0, 1 }; // 상좌하우
 
-
-struct Shark{
-    int size;
-    int eat_num;
+struct Fish {
+	int dist, x, y;
 };
 
-vector<pair<int,int>> fishes; 
 
-int main(void){
-    ios::sync_with_stdio(0), cin.tie(0);
-    
-    cin >> n;
-    queue<pair<int,int>> q; 
-    Shark shark;
-    shark.size = 2;
-    shark.eat_num = 0;
-    
-    for(int i = 0; i < n; i++){
-        for(int j = 0 ; j < n; j++){
-            cin >> board[i][j];
-            if(board[i][j] == 9) q.push({i,j}); // 상어 좌표
-        }
-    }
+bool OOB(int x, int y) {
+	return (x < 0 || x >= n || y < 0 || y >= n);
+}
 
-    
-    int ans = 0;
-    // bfs를 돌리면서 먹을수 있는 가장 가까운 물고기에 도달시 종료한다면 ? (즉 이를 한번에 처리) 
-    // 거리, 좌표정보를 같이 갖고있어야하므로 tuple 사용이 적합
-    while(1){
-    
-        for(int i = 0 ; i < n; i++)  fill(dist[i], dist[i]+n, -1);
-        
-        auto cur = q.front(); 
-        board[cur.X][cur.Y] = 0; // 상어이동
-        dist[cur.X][cur.Y] = 0;
-        
-        tuple<int,int,int> mn = {0x7f7f7f7f, 0x7f7f7f7f, 0x7f7f7f7f};
-        
-        while(!q.empty()){
-            auto cur = q.front(); q.pop();
-            if(dist[cur.X][cur.Y] >= get<0>(mn)) { while(!q.empty()) q.pop(); break; }
-            for(int dir = 0; dir < 4; dir++){
-                int nx = cur.X + dir[dx];
-                int ny = cur.Y + dir[dy];
-                if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-                if(board[nx][ny] > shark.size || dist[nx][ny] >= 0) continue;
-                dist[nx][ny] = dist[cur.X][cur.Y] + 1;
-                q.push({nx,ny});
-                if(board[nx][ny] < shark.size && board[nx][ny] != 0) 
-                    mn = min(mn, {dist[nx][ny], nx, ny});
-            }
-        } // 가까운 물고기를 찾음
-        
-        
-        
-        int min_dist, x, y; 
-        tie(min_dist, x, y) = mn; 
-        if(min_dist == 0x7f7f7f7f) {cout << ans; return 0; } // 가까운 물고기가 없음 
-        
-        shark.eat_num++;
-        if(shark.eat_num >= shark.size) { shark.size++; shark.eat_num = 0;}
-        ans += dist[x][y];
-        board[x][y] = 9; 
-        q.push({x, y});
-  
-    }
+bool isPrior(Fish& candi, int nx, int ny) {
+	return (candi.dist > dist[nx][ny] || (candi.dist == dist[nx][ny] && candi.x > nx)
+		|| (candi.dist == dist[nx][ny] && candi.x == nx && candi.y > ny));
+}
+int eatFish() {
+	int t = 0;
+	queue<pair<int, int>> q;
+
+	for (int i = 0; i < n; i++)
+		fill(dist[i], dist[i] + n, -1);
+
+	dist[sx][sy] = 0;
+	q.push({ sx,sy });
+
+
+	Fish candi = { 0x7f7f7f7f, 0x7f7f7f7f, 0x7f7f7f7f };
+
+	// 1. 먹을 수 있는 물고기 후보탐색
+	while (!q.empty()) {
+		auto cur = q.front(); q.pop();
+
+		if (dist[cur.X][cur.Y] > candi.dist) break; // 제일 가까운 후보 물고기보다 탐색거리가 커지면
+
+		for (int dir = 0; dir < 4; dir++) {
+			int nx = cur.X + dx[dir];
+			int ny = cur.Y + dy[dir];
+			if (OOB(nx, ny)) continue;
+			if (board[nx][ny] > ss || dist[nx][ny] >= 0) continue;
+
+			dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+			q.push({ nx,ny });
+
+			if (board[nx][ny] < ss && board[nx][ny] > 0) { // candi
+				if (isPrior(candi, nx, ny)) 
+					candi = { dist[nx][ny], nx, ny };
+			}
+		}
+	}
+	
+	// 2. 후보 물고기 섭취
+	if (candi.dist != 0x7f7f7f7f) {
+		board[sx][sy] = 0;
+		board[candi.x][candi.y] = 9;
+		sx = candi.x; sy = candi.y;
+		eaten++;
+		if (eaten == ss) { eaten = 0; ss++; }
+		return candi.dist;
+	}
+
+	return 0;
+}
+
+
+
+
+int main(void) {
+	cin >> n; 
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> board[i][j];
+			if (board[i][j] == 9) {
+				sx = i; sy = j;
+			}
+		}
+	}
+
+	int ans = 0;
+	while (1) {
+		int t = eatFish();
+		if (t == 0) break; 
+		ans += t;
+	}
+
+	cout << ans;
+	
 }
