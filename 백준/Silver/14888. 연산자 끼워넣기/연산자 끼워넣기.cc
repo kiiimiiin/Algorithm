90a6df9703a3acf719347e893e36206781433eec
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-07-15 20:18:48 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -1,36 +1,49 @@
-#include <bits/stdc++.h>
-using namespace std; 
-int n,x;
-int num[12];
-int ops[12]; 
-int mx= -0x7f7f7f7f, mn = 0x7f7f7f7f; 
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int arr[12];
+int op[4];
+int n;
+int mx = -0x7f7f7f7f; 
+int mn = 0x7f7f7f7f;
 
-int main(void){
-    
-    ios::sync_with_stdio(0), cin.tie(0); 
-    cin >> n;
-    for(int i = 0 ; i < n; i++) cin >> num[i];
-    
-    for(int i = 0, idx = 0; i < 4; i++){
-        cin >> x; 
-        while(x--) ops[idx++] = i; 
-    } // ops : 0 0 1 1 1 2 2 (n - 1 개)
-    
-    do{
-        int ans = num[0];
-        for(int i = 0; i < n - 1; i++){
-            switch(ops[i]){
-                case 0: {ans += num[i+1]; break;}
-                case 1: {ans -= num[i+1]; break;}
-                case 2: {ans *= num[i+1]; break;}
-                case 3: {ans /= num[i+1]; break;}
-            }
-        }
-        mx = max(mx, ans);
-        mn = min(mn, ans);
-        
-    }while(next_permutation(ops, ops + n - 1));
-    
-    cout << mx << '\n' << mn;
-    
+void dfs(int k, int res) {
+	if (k == n - 1) {
+		mx = max(mx, res);
+		mn = min(mn, res);
+		return;
+	}
+
+	for (int i = 0; i < 4; i++) {
+		if (op[i] <= 0) continue;
+		op[i]--;
+
+		if (i == 0)
+			dfs(k + 1, res + arr[k + 1]);
+		else if (i == 1)
+			dfs(k + 1, res - arr[k + 1]);
+		else if (i == 2) 
+			dfs(k + 1, res * arr[k + 1]);
+		else if (i == 3)
+			dfs(k + 1, res / arr[k + 1]);
+
+		op[i]++;
+	}
+}
+
+
+int main(void) {
+	ios::sync_with_stdio(0), cin.tie(0);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) cin >> arr[i];
+
+
+	for (int i = 0; i < 4; i++) {
+		cin >> op[i];
+	}
+
+	dfs(0, arr[0]);
+	cout << mx << '\n' << mn;
 }
