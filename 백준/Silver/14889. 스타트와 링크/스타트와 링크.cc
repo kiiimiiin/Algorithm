c2beea30b266e42237a7c089f29248d4ec39a7f6
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-07-17 16:11:48 +0900

    [Silver I] Title: 스타트와 링크, Time: 32 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -1,42 +1,13 @@
 #include <iostream>
 #include <math.h>
+#include <vector>
+#include <algorithm>
 using namespace std;
 int s[22][22];
-bool candi[22];
+int t1[11], t2[11];
 int n;
 int mn = 0x7f7f7f7f;
 
-void dfs(int k, int st) {
-	if (k == n / 2) {
-		int sum1, sum2, idx1, idx2;
-		int team1[11], team2[11];
-		
-		sum1 = sum2 = idx1 = idx2 = 0;
-		
-
-		for (int i = 0; i < n; i++) {
-			if (candi[i]) team1[idx1++] = i;
-			else team2[idx2++] = i;
-		}
-
-			
-		for (int i = 0; i < n / 2; i++) {
-			for (int j = i + 1; j < n / 2; j++) {
-				sum1 += s[team1[i]][team1[j]] + s[team1[j]][team1[i]];
-				sum2 += s[team2[i]][team2[j]] + s[team2[j]][team2[i]];
-			}
-		}
-
-		mn = min(mn, abs(sum1 - sum2));
-		return;
-	}
-
-	for (int i = st; i < n; i++) {
-		candi[i] = true;
-		dfs(k + 1, i + 1);
-		candi[i] = false;
-	}
-}
 
 int main() {
 	ios::sync_with_stdio(0), cin.tie(0);
@@ -48,6 +19,30 @@ int main() {
 		}
 	}
 
-	dfs(0, 0);
-	cout << mn;
+	vector<int> candi;
+	for (int i = 0; i < n; i++) {
+		if (i < n / 2) candi.push_back(0);
+		else candi.push_back(1);
+	}
+
+	do {
+		int idx1, idx2, sum1, sum2;
+		sum1 = sum2 = idx1 = idx2 = 0;
+
+		for (int i = 0; i < n; i++) {
+			if (candi[i] == 0) t1[idx1++] = i;
+			else t2[idx2++] = i;
+		}
+
+		for (int i = 0; i < n / 2; i++) {
+			for (int j = i + 1; j < n / 2; j++) {
+				sum1 += s[t1[i]][t1[j]] + s[t1[j]][t1[i]];
+				sum2 += s[t2[i]][t2[j]] + s[t2[j]][t2[i]];
+			}
+		}
+
+		mn = min(mn, abs(sum1 - sum2));
+	} while (next_permutation(candi.begin(), candi.end()));
+
+	cout << mn; 
 }
