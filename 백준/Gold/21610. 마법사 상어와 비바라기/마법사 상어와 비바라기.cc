ebbc5ad632720cd2e9afc4b43c875a161609dceb
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-08-04 19:38:10 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 4 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.cc
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.cc
@@ -1,99 +1,111 @@
-// 1. 구름 생성, 이동 , 사라짐
 #include <iostream>
-#include <vector>
+#include <queue>
+#include <utility>
+#include <tuple>
 #define X first
 #define Y second
+const int dx[8] = { 0,-1,-1,-1,0,1,1,1 };
+const int dy[8] = { -1,-1,0,1,1,1,0,-1 };
 using namespace std;
-int dx[8] = { 0, -1, -1, -1, 0, 1, 1, 1 };
-int dy[8] = { -1, -1, 0, 1, 1, 1, 0, -1 };
+
 int board[52][52];
-int vis[52][52];
-int n, m;
-vector<pair<int, int>> clouds;
+int cloud[52][52];
+int n, m, d, s;
 
 pair<int, int> ProcessOOB(int x, int y) {
-	int nx, ny;
-	if (x < 0) nx = n + x;
-	else if (x >= n) nx = x - n; // x == n -> nx == 1
-	else nx = x;
+	if (x < 0) x = n + x;
+	else if (x >= n) x = x - n;
 
-	if (y < 0) ny = n + y;
-	else if (y >= n) ny = y - n;
-	else ny = y;
-
-	return { nx, ny };
+	if (y < 0) y = n + y;
+	else if (y >= n) y = y - n;
+	
+	return { x, y };
 }
 
-void CopyWater(vector<pair<int, int>> water) {
+void doMagic(int d, int s) {
+	int temp[52][52] = {};
 
-	for (auto pos : water) {
-		int cnt = 0; // 대각선 방향 물 바구니 수
-		for (int dir = 1; dir < 8; dir += 2) {
-			int nx = pos.X + dx[dir];
-			int ny = pos.Y + dy[dir];
-			if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-			if (board[nx][ny]) cnt++;
-		}
-		board[pos.X][pos.Y] += cnt;
-	}
-}
-
-
-void Command(int d, int s) {
-	// 1. 모든구름 d방향 s칸이동 , 비내리기, 물 양 증가
-	vector<pair<int, int>> v;
-	int removed[52][52] = {};  // 구름이 삭제된 칸
-
-	for (auto c : clouds) {
-		int nx = c.X + (s % n) * dx[d];
-		int ny = c.Y + (s % n) * dy[d];
-		auto ret = ProcessOOB(nx, ny);
-		board[ret.X][ret.Y]++;
-		removed[ret.X][ret.Y] = 1;
-		v.push_back({ ret.X, ret.Y }); // 물이 증가한 칸 저장
-	}
-
-	clouds.clear();
-
-	// 2. 물복사
-	CopyWater(v);
-
-	// 3. 바구니 칸에서 구름 생성
+	// 1. 구름 이동, 비내리기
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			if (board[i][j] >= 2 && !removed[i][j]) {
-				clouds.push_back({ i,j });
-				board[i][j] -= 2;
+			if (cloud[i][j]) {
+				int nx = i + ( s * dx[d] ) % n;
+				int ny = j + ( s * dy[d] ) % n;
+				tie(nx, ny) = ProcessOOB(nx, ny);
+				temp[nx][ny]++;
+				board[nx][ny]++;
+				cloud[i][j] = 0;
 			}
 		}
 	}
+
+	// 2. 물증가 칸 마법시전
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (temp[i][j]) {
+				for (int dir : { 1, 3, 5, 7}) {
+					int nx = i + dx[dir];
+					int ny = j + dy[dir];
+					if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+					if (board[nx][ny]) {
+						board[i][j]++;
+					}
+				}
+			}
+		}
+	}
+
+	// 3. 구름 재 생성
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (board[i][j] >= 2 && !temp[i][j]) {
+				board[i][j] -= 2;
+				cloud[i][j] = 1;
+			}
+		}
+	}
+
 }
 
+int getAns() {
+	int ret = 0; 
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			ret += board[i][j];
+		}
+	}
+	return ret;
+}
+
+
 int main(void) {
-	ios::sync_with_stdio(0), cin.tie(0);
+	ios::sync_with_stdio(0), cin.tie(0); 
 
 	cin >> n >> m;
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			int x;
-			cin >> x;
-			board[i][j] = x; // 물양
+			cin >> board[i][j];
 		}
 	}
 
-	clouds.push_back({ n - 1,0 }); clouds.push_back({ n - 1,1 });
-	clouds.push_back({ n - 2,0 }); clouds.push_back({ n - 2,1 });
+	cloud[n - 1][0] = cloud[n - 1][1] = 
+		cloud[n - 2][0] = cloud[n - 2][1] = 1;
+
+
 
 	while (m--) {
-		int d, s;
-		cin >> d >> s;
-		Command(d - 1, s);
+		cin >> d >> s; 
+		d--;
+		doMagic(d, s);
 	}
 
-	int ans = 0;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			ans += board[i][j];
+	int ans;
+	ans = getAns();
 	cout << ans;
+	
 }
+
+/*
+	구름 보드
+*/
