0a50215cb071289d90f80b411f639a4c7c8a051c
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-08-05 18:07:27 +0900

    [Gold IV] Title: 마법사 상어와 파이어볼, Time: 16 ms, Memory: 3380 KB -BaekjoonHub

--- 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.cc
+++ 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.cc
@@ -1,151 +1,111 @@
 #include <iostream>
-#include <algorithm>
-#include <queue>
-#define X first
-#define Y second
+#include <vector>
+#include <tuple>
 using namespace std;
-struct Fire {
-    int m;
-    int d;
-    int s;
+
+struct Fireball {
+	int x, y, m, s, d;
 };
-int dx[8] = { -1, -1, 0, 1, 1, 1, 0, -1 };
-int dy[8] = { 0, 1, 1, 1, 0, -1, -1, -1 };
-vector<Fire> board[52][52]; // 파이어볼 정보를 담는 코드 
-int N,M,K; // M : 파이어볼 개수 , K : 명령수
 
-/* 1.이동
-새로운 구조체 벡터 배열에 이동한 보드를 복사
-이동한 보드를 원래보드에 복사 (이동완료한 배열 만듦)
+vector<int> board[52][52];
+vector<Fireball> fire; 
+const int dx[8] = { -1, -1, 0, 1, 1, 1, 0, -1 };
+const int dy[8] = { 0, 1, 1, 1, 0, -1, -1, -1 };
 
-2. 합치기 나누기
-구벡배를 순화해서 각 자리 불 합함(원래 불 제거)
-나누어지는 불 정보들을 구하고
-나누어진 불을 다시 그 벡터에 삽입 */
+int n, m, k;
 
+pair<int, int> ProcessOOB(int x, int y) {
+	if (x < 0) x = n + x;
+	else if (x >= n) x = x - n;
 
-void TestFire(){
-    //파이어볼 위치에 대한질량 확인
-    cout << "Test Fire, m:\n";
-    for(int i = 0 ; i < N ; i++){
-        for(int j = 0 ; j < N; j++){
-            if(board[i][j].size())
-                cout << board[i][j].front().m << ' ';
-            else
-                cout << 0 << ' ';
-        }
-        cout << '\n';
-    }
+	if (y < 0) y = n + y;
+	else if (y >= n) y = y - n;
+
+	return { x, y };
 }
 
-pair<int,int> ProcessOOB(int x, int y){
-    // 바깥으로 나가있으면 연결된 보드처리
-    int nx, ny; 
-    if(x < 0) nx = N + x ; // x = -1이면 N-1로, -2 이면 N-2로
-    else if(x >= N) nx = x - N ; // x = N 이면 0으로, N+1이면 1로 
-    else nx = x;
-    
-    if(y < 0) ny = N + y  ;
-    else if(y >= N) ny = y - N ;
-    else ny = y;
-    
-    return {nx,ny};
+void Move() {
+	for (int i = 0; i < fire.size(); i++) {
+		if (fire[i].m <= 0) continue;
+		int nx = fire[i].x + ( fire[i].s % n ) * dx[fire[i].d];
+		int ny = fire[i].y + ( fire[i].s % n ) * dy[fire[i].d];
+		tie(nx, ny) = ProcessOOB(nx, ny);
+		board[nx][ny].push_back(i);
+	}
 }
 
-void Copy(vector<Fire> desc[52][52], vector<Fire> src[52][52]){
-    for(int i = 0 ; i < N; i++){
-        for(int j = 0 ; j < N; j++){
-            desc[i][j] = src[i][j]; 
-        }
-    }    
+void Merge() {
+	vector<Fireball> temp;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (board[i][j].empty()) continue;
+
+			if (board[i][j].size() == 1) {
+				int idx = board[i][j].front();
+				temp.push_back({ i , j, fire[idx].m, fire[idx].s, fire[idx].d });
+			}
+			else if (board[i][j].size() >= 2) {
+				int mm = 0;
+				int ms = 0;
+				int flag = 0;
+
+				vector<int> dir;
+				for (auto idx : board[i][j]) {
+					mm += fire[idx].m;
+					ms += fire[idx].s;
+					flag += fire[idx].d % 2;
+				}
+				mm /= 5;
+				ms /= board[i][j].size();
+				 
+				if (flag == board[i][j].size() || flag == 0)
+					dir = { 0,2,4,6 };
+				else
+					dir = { 1,3, 5,7 };
+				
+				for (int d : dir) {
+					if (mm <= 0) break;
+					temp.push_back({ i, j, mm, ms, d });
+				}		
+			}
+			board[i][j].clear();
+		}
+	}
+	fire = temp;
+}
+
+int getAns() {
+	int ret = 0;
+	for (auto f : fire) {
+		if (f.m <= 0) continue;
+		ret += f.m;
+	}
+	return ret;
+}
+
+int main(void) {
+
+	cin >> n >> m >> k;
+
+	while (m--) {
+		int x, y, m, s, d;
+		cin >> x >> y >> m >> s >> d;
+		x--; y--;
+		fire.push_back({ x, y, m, s, d });
+	}
+
+	while (k--) {
+		Move();
+		Merge();
+	}
+
+	int ans = getAns();
+	cout << ans;
 }
 
 
-int main(void){
-    ios::sync_with_stdio(0), cin.tie(0);
-    
-    cin >> N >> M >> K;
-    for(int i = 0 ; i < M ; i++){
-        int x, y, m , s , d;
-        cin >> x >> y >> m >> s >> d;
-        Fire fire;
-        fire.m = m; fire.s = s; fire.d = d;
-        board[x-1][y-1].push_back(fire);
-    }
-    
-    
+/*
 
-    
-    while(K--){
-        // 1. 존재하는 파이어볼 이동
-        
-        vector<Fire> c_board[52][52];
-        
-        for(int i = 0 ; i < N; i++){
-            for(int j = 0 ; j < N; j++){
-                if(board[i][j].empty()) continue; // 파이어볼이 비어있음 
-                
-                auto fires = board[i][j];
-                
-                for(auto fire : fires){ 
-                    int nx = i +  ( fire.s % N ) * dx[fire.d] ; 
-                    int ny = j + ( fire.s % N ) * dy[fire.d] ;
-                    auto ret = ProcessOOB(nx,ny);
-                    if(ret.X < 0 || ret.X >= N || ret.Y < 0 || ret.Y >= N)
-                    { exit(0); }
-                    c_board[ret.X][ret.Y].push_back(fire);
-                }
-            }
-            
-        }
-        
-        Copy(board, c_board);
-
- 
-        //2. 파이어볼 나누기 (파이어볼 탐색 후 나눔)
-
-        
-        for(int i = 0 ; i < N ; i++){
-            for(int j = 0 ; j < N; j++){
-                if(board[i][j].size() >= 2){ // 2개 이상의 파이어볼 칸
-                    int cnt = board[i][j].size();
-                    int sum_m , sum_s, d_is_all_same;
-                    sum_m = sum_s = d_is_all_same = 0;
-                    for(auto fire : board[i][j]){
-                        sum_m += fire.m;
-                        sum_s += fire.s;
-                        d_is_all_same += ( fire.d % 2 ); // 홀수면 1누적 짝수면 0
-                    }
-                    
-                    vector<int> dirs; 
-                    
-                    if(d_is_all_same == cnt || d_is_all_same == 0)
-                        dirs = { 0,2,4,6 };
-                    else
-                        dirs = { 1, 3, 5, 7};
-                    
-                    board[i][j].clear();
-                    int new_m = sum_m / 5;
-                    if(new_m == 0 ) continue;
-                    int new_s = sum_s / cnt;
-                    for(int dir : dirs){
-                        Fire new_fire;
-                        new_fire.m = new_m; new_fire.s = new_s;
-                        new_fire.d = dir; 
-                        board[i][j].push_back(new_fire);
-                    }
-                }
-            }
-        }
-    }
-    
-    int ans = 0;
-    for(int i = 0; i < N; i++){
-        for(int j = 0 ; j < N; j++){
-            if(board[i][j].empty()) continue;
-            for(auto fire : board[i][j]) ans += fire.m;
-        }
-    }
-    cout << ans;
-    return 0;
-}
+	파이어볼 구조체 배열과
+	인덱스 보드
+*/
