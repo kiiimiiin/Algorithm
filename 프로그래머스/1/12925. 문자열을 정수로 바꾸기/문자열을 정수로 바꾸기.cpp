500ca3476c9ecd0ea91935ed341387d128e8c43f
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-08-08 15:27:52 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cpp
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cpp
@@ -1,11 +1,22 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 int solution(string s) {
   
-    int answer = stoi(s);  
+    int answer = 0;
+    int m = 1;
+    for(int i = 0 ; i < s.length(); i++){
+        if(s[i] == '-'){
+            m = -1;
+        }
+        else if(s[i] >= '0' && s[i] <= '9'){
+            answer *= 10;
+            answer += s[i] - 48;
+        }
+    }
     
-    return answer;
+    return answer * m;
 }
