c4f037f4e9e37c867cc30f3c3850a58a711a3f8d
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-08-08 16:56:28 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -7,12 +7,11 @@ using namespace std;
 string solution(vector<string> seoul) {
     string answer = "";
     int idx = 0;
-    answer += "김서방은 ";
     
     for(int i = 0 ; i < seoul.size(); i++){
         if(seoul[i] == "Kim") idx = i;
     }
     
-    answer += to_string(idx) + "에 있다";
+    answer += "김서방은 " + to_string(idx) + "에 있다";
     return answer;
 }
