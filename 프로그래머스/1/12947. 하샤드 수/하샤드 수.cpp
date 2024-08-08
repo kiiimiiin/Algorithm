338fbaafa2a837438a477924a4488aa5b2b5510a
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-08-08 16:29:22 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+ 
+    int tmp = x;
+    int sum = 0;
+    while(tmp){
+        sum += tmp % 10;
+        tmp /= 10; 
+    }
+
+    return x % sum == 0 ? true : false;
+}
