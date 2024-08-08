f5dcc3b0ecd69259aa0d83ce26086f2b3b7c4cfd
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-08-08 17:28:26 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -1,16 +1,17 @@
 #include <string>
 #include <algorithm> 
 #include <vector>
-
 using namespace std;
+int arr[10];
 
 int solution(vector<int> numbers) {
     int answer = 0;
+    for(auto n : numbers){
+        arr[n] = true;
+    }
     
-    for(int i = 0; i <= 9; i++){
-        auto it = find(numbers.begin(), numbers.end(), i);
-        if(it != numbers.end()) continue; 
-        answer+= i; 
+    for(int i = 0 ; i <= 9; i++){
+        if(arr[i] == false) answer += i;
     }
     
     return answer;
