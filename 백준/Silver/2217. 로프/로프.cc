69bda2ff77365dc92117ca3693dca899991a52cd
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-08-11 13:34:33 +0900

    [Silver IV] Title: 로프, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.cc
+++ 백준/Silver/2217. 로프/로프.cc
@@ -1,38 +1,30 @@
 #include <iostream>
-#include <vector>
-#include <algorithm> 
+#include <algorithm>
 using namespace std;
-int n , t;
 int rope[100002];
+int n;
 
-int main()
-{
-   cin >> n ;
-   
-   for( int i = 1 ; i <= n; i++){
-       int rw;
-       cin >> rw;
-       rope[i] = rw;
-   }
-   
-   sort(rope + 1, rope + n + 1, greater<int>()); 
-   
-   int mx = -0x7f7f7f7f;
-
-   for(int i = 1; i <= n; i++){
-       mx = max(mx, rope[i] * i );
-   }
-   
-   cout << mx;
+int main(void){
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> n;
+    
+    for(int i = 1 ; i <= n; i++)
+        cin >> rope[i];
+        
+    sort(rope + 1, rope + n + 1, greater<int>());
+    
+    int mx = -0x7f7f7f7f;
+    for(int i = 1 ; i <= n; i++){
+        mx = max(mx, rope[i] * i);
+    }
+    
+    cout << mx;
     
 }
 
 /*
-    로프가 들어 올릴 수 있는 물체의 최대 중량
-    k개 로프를 사용해 중량 w인 물체를 들어올릴 때 로프에는 고르게 w/k가 걸림
+    큰 로프를 택해나갈때 버티는 최대중량이라고 가정
     
-    11 12 13 일 때 최대 중량 -> 33 /3 = 11, 11 * 3 = 33
-    
-    큰 수 부터 작은수 순회, 현재까지 택한 수의 값과 로프개수를 곱
-    
+    10, 15 택하면 10 씩가해짐
+    5, 10 , 15 택하면 5씩가해짐
 */
