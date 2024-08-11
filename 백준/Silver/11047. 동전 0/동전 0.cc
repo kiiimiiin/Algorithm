bb5ec845e13079803711600605ef1d3488d34425
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-08-11 12:31:39 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -1,28 +1,24 @@
 #include <iostream>
 using namespace std;
-int n , k;
-int arr[12];
+int n, k;
+int arr[11];
 
-int main()
-{
-   cin >> n >> k;
-   
-   for(int i = 1 ; i <= n; i++){
-       cin >> arr[i];
-   }
-   
-   int ans = 0; 
-   for(int i = n; i >= 1; i--){
-       if(arr[i] > k) continue;
-       ans += k / arr[i]; 
-       k %= arr[i];
-   }
-   
-   cout << ans;
+
+int main(void){
+    ios::sync_with_stdio(0), cin.tie(0);
+    
+    cin >> n >> k;
+    for(int i = 1; i <= n; i++){
+        cin >> arr[i];
+    }
+    
+    
+    int ans = 0;
+    for(int i = n; i >= 0; i--){
+        if( k == 0) break;
+        ans += k / arr[i];
+        k = k % arr[i];
+    }
+    cout << ans;
     
 }
-
-/*
-    가정 ) 높은 금액의 동전을 최대한 많이 쓴다. 
-
-*/
