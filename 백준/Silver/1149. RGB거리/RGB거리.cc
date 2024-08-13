063c6c75264609843c8a2a37a31dfbba2905b04a
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-08-13 15:32:53 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,34 +1,31 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int n;
+int c[1002][3];
 int d[1002][3];
-int cost[1002][3];
+int n;
 
-int main(){
-    cin >> n;
-    
-    for(int i = 1; i <= n; i++){
-        cin >> cost[i][0] >> cost[i][1] >> cost[i][2];
+int main(void){
+    cin >> n; 
+    for(int i = 1 ; i <= n; i++){
+        cin >> c[i][0] >> c[i][1] >> c[i][2];
     }
     
-    d[1][0] = cost[1][0];
-    d[1][1] = cost[1][1];
-    d[1][2] = cost[1][2];
+    d[1][0] = c[1][0];
+    d[1][1] = c[1][1];
+    d[1][2] = c[1][2];
     
-    for(int i = 1; i <= n; i++){
-        d[i][0] = min(d[i-1][1], d[i-1][2]) + cost[i][0];
-        d[i][1] = min(d[i-1][0], d[i-1][2]) + cost[i][1];
-        d[i][2] = min(d[i-1][0], d[i-1][1]) + cost[i][2];
+    for(int i = 2; i <= n; i++){
+        d[i][0] = min(d[i-1][1], d[i-1][2]) + c[i][0];
+        d[i][1] = min(d[i-1][0], d[i-1][2]) + c[i][1];
+        d[i][2] = min(d[i-1][0], d[i-1][1]) + c[i][2];
     }
     
-    cout << min({d[n][0],d[n][1],d[n][2]});
+    cout << min({d[n][0], d[n][1], d[n][2]});
 }
 
+
 /*
-
-    d[i][j] : i번째 집까지 칠하는 비용의 최솟값, 단 j는 칠한 색깔
-    
-    d[k][0] = min(d[k-1][1], d[k-1][2]) + cost[k][0]
-
+    d[i][j] : i번째 집을 j로 칠했을 때 최소 비용
+    d[i][0] = min(d[i-1][1], d[i-1][2]) + cost[i][0]
 */
