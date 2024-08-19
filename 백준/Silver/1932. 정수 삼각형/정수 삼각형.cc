5e993aee72ed2ad8fa9f3221d54ecb78e165cba5
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-08-19 10:03:14 +0900

    [Silver II] Title: 정수 삼각형, Time: 8 ms, Memory: 3004 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,29 +1,33 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-int arr[502][502]; // 위에서 부터 내려오는 층수, 해당 층에 대한 원소의 인덱스
-int dp[502][502]; // 위에서 i-1번째 층 까지의 최대합 , j는 i번쨰 층에 대한 원소 인덱스
-int n;
+int d[502][502];
+int n, x;
 
-int main()
-{
+int main(){
     ios::sync_with_stdio(0), cin.tie(0);
+    
     cin >> n;
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < i + 1 ; j++)
-        {
-            cin >> arr[i][j];
+    
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cin >> x;
+            d[i][j] = max(d[i-1][j-1], d[i-1][j]) + x; 
         }
     }
-    dp[0][0] = arr[0][0];
-    for(int i = 1; i < n ; i++)
-    {
-        dp[i][0] = dp[i-1][0] + arr[i][0];
-        dp[i][i] = dp[i-1][i-1] + arr[i][i];
-        if( i >= 2)
-            for(int j = 1; j <= i-1 ; j++ )
-            dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + arr[i][j];
-    }
-
-    cout << *max_element(dp[n-1], dp[n-1] + n);
+    
+    cout << *max_element(d[n] + 1, d[n] + n + 1); 
 }
+
+
+/*
+    d[i][j] : i번째 행에서 j열을 택했을 때까지의 최대값
+
+    d[1][1] = 7
+    
+    d[2][1] = max(d[1][0] ,d[1][1] ) + num[i][j];
+    d[2][2] = max(d[1][1] ,d[1][2] ) + num[i][j];
+    
+    d[i][j] = d[i-1][j-1] + d[i-1][j];
+     
+*/
