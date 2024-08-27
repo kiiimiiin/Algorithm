ba61bdf26e148403a85398a3aa504fbc9bb179f8
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-08-27 20:56:51 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,33 +1,31 @@
 #include <string>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
-    vector<int> answer;
-
-    queue<int> q;
-
-    for(int i = 0; i < progresses.size(); i++) {
-        int temp = (100-progresses[i]) % speeds[i];
-        if(temp == 0)
-            q.push((100-progresses[i]) / speeds[i]);
-        else
-            q.push((100-progresses[i]) / speeds[i] + 1);
-    }
-
-    while(!q.empty()) {
-        int cnt = 1;
-        int cur = q.front();
-        q.pop();
-
-        while(cur >= q.front() && !q.empty()) {
-            q.pop();
-            cnt++;
+    vector<int> answer(1, 1);
+    int idx = 0; 
+    int mxDate = ( 100 - progresses[0] ) / speeds[0];
+    if((100 - progresses[0]) % speeds[0]) mxDate++;
+    
+    for(int i = 1; i < progresses.size(); i++){
+        int date = ( 100 - progresses[i] ) / speeds[i]; 
+        if((100 - progresses[i]) % speeds[i]) date++;
+        if(date <= mxDate) answer[idx]++;
+        else{
+            mxDate = date;
+            answer.push_back(1);
+            idx++;
         }
-
-        answer.push_back(cnt);
+        
     }
     return answer;
 }
+
+/*
+    7일 3일 9일
+    앞에서부터 걸리는 일자수 계산해나감
+    뒤에 나타나는게 걸리는 일자수보다 작거나 같을때 + 1, 클때 새로운 원소
+
+*/
