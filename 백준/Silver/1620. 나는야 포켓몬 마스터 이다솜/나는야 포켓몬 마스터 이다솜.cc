3f46fee67ed58b19026c8efd4e1a938c327576c0
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-08-28 16:48:13 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 104 ms, Memory: 20532 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,36 +1,35 @@
 #include <iostream>
 #include <unordered_map>
-#include <string>
-#include <cctype> // isdigit
+#include <string> 
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main(void){
     ios::sync_with_stdio(0), cin.tie(0);
     
     int n, m;
-    string s;
-    unordered_map<string, int> dict_s;
-    unordered_map<int, string> dict_i;
+    unordered_map<string, int> s2i;
+    string i2s[100002];
     
     cin >> n >> m;
     
-    for(int i = 1 ; i <= n ; i++){
+    for(int i = 1 ; i <= n; i++){
+        string name;
+        cin >> name;
         
-        cin >> s;
-        dict_s[s] = i;
-        dict_i[i] = s;
+        s2i[name] = i;
+        i2s[i] = name;
     }
     
-    for(int i = 0; i < m; i++){
-        string init;
-        cin >> init;
-        if(isdigit(init[0])){
-            int num = stoi(init);
-            cout << dict_i[num] << '\n';
+    for(int i = 0 ; i < m; i++){
+        string x;
+        cin >> x;
+        if(isdigit(x[0])){
+            cout << i2s[stoi(x)] << '\n';
         }
         else{
-            cout << dict_s[init] << '\n';
+            cout << s2i[x] << '\n';
         }
-        
     }
 }
