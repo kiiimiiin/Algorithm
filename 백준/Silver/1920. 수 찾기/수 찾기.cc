c3598f6d5362a22ae2b2cc685eb1afd84609bfda
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-09-03 16:58:17 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int a[100005];
+int n, m;
+
+int binarySearch(int target){
+    int st = 0;
+    int en = n-1;
+    while(st <= en){
+        int mid = (st + en) / 2;
+        if(a[mid] < target)
+            st = mid + 1;
+        else if(a[mid] > target)
+            en = mid - 1;
+        else
+            return 1;
+    }
+    return 0;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    
+    cin >> n;
+    for(int i = 0 ; i < n; i++) cin >> a[i];
+    sort(a, a+n);
+    
+    cin >> m;
+    while(m--){
+        int x;
+        cin >> x;
+        cout << binarySearch(x) << '\n'; 
+    }
+    
+    return 0;
+}
+
