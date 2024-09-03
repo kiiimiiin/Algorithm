e72cf72d73b3654f8f25471d9cfb3361154a5260
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-09-03 18:19:08 +0900

    [Silver IV] Title: 차집합, Time: 236 ms, Memory: 9132 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.cc
+++ 백준/Silver/1822. 차집합/차집합.cc
@@ -8,7 +8,7 @@ int na, nb;
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0);
-    
+    vector<int> finded;
     cin >> na >> nb;
     for(int i = 0; i < na; i++)
         cin >> a[i];
@@ -17,25 +17,18 @@ int main()
 
     sort(b, b + nb);
     
-    vector<int> finded;
-    
     for(int i = 0 ; i < na; i++){
         if(!binary_search(b, b + nb, a[i])){
             finded.push_back(a[i]);
         }
     }
     
-    if(!finded.empty()){
-        sort(finded.begin(), finded.end());
-        
-        cout << finded.size() << '\n';
-        for(auto x : finded)
-            cout << x << ' ';
+    sort(finded.begin(), finded.end());
+    
+    cout << finded.size() << '\n';
+    
+    for(auto x : finded)
+        cout << x << ' ';
             
-    } else{
-        cout << 0;
-    }
-    
-    
 }
 
