f435e9c5083a089c82eca4be681d18270e4f0021
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-09-16 14:27:43 +0900

    [level 2] Title: 숫자의 표현, Time: 0.03 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -6,25 +6,24 @@ using namespace std;
 int solution(int n) {
     int answer = 1;
     
-    for(int i = 1; i <= (n / 2 + 1) ; i++){
-        int sum = 0;
-        for(int j = i; j <= (n / 2 + 1); j++){
+    for(int i = 1; i <= n / 2; i++){
+        int sum = i;
+        for(int j = i + 1; j < n; j++){
             sum += j;
-            if(sum == n){
-                answer++;
-                break;
-            }
-            else if( sum > n) break;
+            if(sum > n) break;
+            if(sum == n) { answer++; break;}
         }
     }
-    
-    return n >= 3 ? answer : 1;
+    return answer;
 }
 
-
 /*
-    연속합을 차례로 구함
-    n과 동일하면 cnt
-    n을 넘어가면 x
+    1부터
+    
+    15 / 2 + 1 = 8 까지
+    
+    16 / 2 = 8 까지
+    
+    1부터 8까지 순회하며 연속합이 15일때마다 cnt 
 
 */
