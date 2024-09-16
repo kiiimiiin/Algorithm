ec67fb2e28b7f5823bf19ab805043e30fd37b80d
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-09-16 14:44:53 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -6,16 +6,24 @@ using namespace std;
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
     
-    int area = brown + yellow;
+    int n = brown + yellow;
     
-    for(int r = 3;;r++){ 
-        if(area % r) continue;
-        int c = area / r; 
+    for(int r = 1; r <= n / 2; r++){
+        if(n % r) continue;
+        int c = n / r;
         if(yellow == (r - 2) * (c - 2)){
             answer.push_back(c);
             answer.push_back(r);
             break;
-        } 
+        }
     }
     return answer;
 }
+
+/*
+    brown + yellow = 전체 격자 개수 n 
+    
+    높이 r 1부터 n / 2 까지 순회하며 그에 맞는 가로 c 찾음
+    가로사이즈에 대해서 yellow = ( height - 2 ) * (c - 2)가 성립을하면 그 때 r, c return
+    
+*/
