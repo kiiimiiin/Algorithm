5dd8882944b8ea7402d568b05467cc44e9f72caa
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-09-21 12:39:21 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 52.38 ms, Memory: 20.5 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -1,36 +1,36 @@
 #include <bits/stdc++.h>
-int sum[2002];
 using namespace std;
+int sum[2004];
 
 int solution(vector<int> elements) {
     int answer = 0;
     int len = elements.size();
+    unordered_set<int> s;
     
-    unordered_set <int> s;
-    
-    for(int i = 0; i < len -1 ; i++)
+    for(int i = 0 ; i < len - 1; i++){
         elements.push_back(elements[i]);
+    }
     
     sum[0] = elements[0];
-    for(int i = 1; i < elements.size(); i++)
+    for(int i = 1; i < elements.size(); i++){
         sum[i] = sum[i - 1] + elements[i];
-        
+        cout << sum[i] << ' ';
+    }
     
     for(int l = 0; l < len; l++){
-        for(int i = 0; i <= len - 1; i++){
-            s.insert( i == 0 ? sum[i + l] : sum[i + l] - sum[i - 1]);
-        } 
-        cout << '\n';
+        s.insert(sum[l]);
+        for(int i = 1; i < len; i++)
+            s.insert(sum[i + l] - sum[i - 1]);
     }
     
     answer = s.size();
-    
+
     return answer;
 }
 
 /*
-    길이 1000
-    
-    1000 * (1000 ) = O(N^2)
-
+    앞과 끝을 이어붙인다
+    7 9 1 1 4 7 9 1 1 4
+    길이별로 배열순회
+    O(n^2)
 */
