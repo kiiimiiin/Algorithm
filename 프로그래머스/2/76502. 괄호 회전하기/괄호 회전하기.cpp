dc45c4268008094612a95eb4d5fd73adf21730f2
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-09-22 12:05:02 +0900

    [level 2] Title: 괄호 회전하기, Time: 1.71 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -3,47 +3,46 @@
 using namespace std;
 
 bool IsTwin(char t, char c){
-    return (t == '[' && c == ']') || (t == '(' && c == ')') || (t == '{' && c == '}');
+    return t == '(' && c == ')' || t == '[' && c == ']' || t == '{' && c == '}' ;
+}
+
+bool IsCorrect(string s, int len){
+    stack<char> st;
+    for(auto c : s){
+        if(c == '[' || c == '(' || c == '{')
+            st.push(c);
+        else{
+            if(st.empty()) return false;
+            if(!IsTwin(st.top(), c)) return false;
+            else st.pop();
+        }
+    }
+    
+    if(!st.empty()) return false;
+    return true;
 }
 
 int solution(string s) {
     int answer = 0;
-    int len = s.length();
-    string temp = s;
-    for(int i = 0; i < len; i++){
-        string temp = s;
-        stack<char> S;
-        rotate(temp.begin(), temp.begin() + i, temp.end());
-        
-        bool flag = true;
-        
-        for(auto c : temp){
-            if(c == '(' || c == '[' || c == '{')
-                S.push(c);
-            else {
-                if(S.empty()) { flag = false; break;}
-                
-                if(IsTwin(S.top(), c)) S.pop();
-                else { flag = false; break;}
-            }
-            
-            if(!flag) break; 
-        }
-        
-        if(!S.empty() && flag) flag = false;
-        if(flag) answer++;
+    int len = s.length(); 
+    
+    for(int i = 0 ; i < len; i++){
+        rotate(s.begin(), s.begin() + 1, s.end());
+        if(IsCorrect(s, len)) answer++;
     }
+    
     return answer;
 }
 
 /*
-    열린괄호 push
+    1칸씩 rotate하며 괄호 check
     
-    닫힌괄호가 나타날때 
-        비어있으면 x
-        짝이맞으면 pop
-        짝이 안맞으면 x
+    stack
+    열린괄호를 처리
+    1. 닫힌괄호 없을때 열린괄호 -> X
+    2. 닫힌괄호와 열린괄호가 다른짝 -> X
+    3. 닫힌괄호와 열린괄호가 같은짝 -> O
     
-    
-    모든 처리를 하고 괄호가 남아있으면 X
+    닫힌괄호가 남아있음
+
 */
