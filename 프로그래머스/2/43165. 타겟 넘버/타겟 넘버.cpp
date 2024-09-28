e5d6dbd8760420ae53b5ebbaaf7527578fa22786
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-09-28 10:30:12 +0900

    [level 2] Title: 타겟 넘버, Time: 3.43 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,22 +1,26 @@
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 int answer;
-bool vis[22];
 
-void dfs(vector<int>& numbers, int target, int sum, int k){
+void dfs(vector<int>& numbers, int target, int total, int k){
     if(k == numbers.size()){
-        if(sum == target) answer++;
-        return ;
+        if(total == target)
+            answer++;
+        return;
     }
     
-    dfs(numbers, target, sum + numbers[k], k + 1);
-    dfs(numbers, target, sum - numbers[k], k + 1);
+    dfs(numbers, target, total + numbers[k], k + 1);
+    dfs(numbers, target, total - numbers[k], k + 1);
 }
 
 int solution(vector<int> numbers, int target) {
     
     dfs(numbers, target, 0, 0);
-    
     return answer;
 }
+
+/*
+    N = 20 -> dfs 
+    
+    수의 끝까지 +, -를 나누며 dfs
+*/
