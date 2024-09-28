b7bf506359f21d5febb1f56bd6cf79475c9b9b6c
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-09-28 14:39:14 +0900

    [level 3] Title: 정수 삼각형, Time: 0.89 ms, Memory: 9.02 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -1,31 +1,31 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <iostream>
+#include <bits/stdc++.h>
+int d[502][502];
 using namespace std;
 
 int solution(vector<vector<int>> triangle) {
     int answer = 0;
-
-    vector<vector<int>> d(triangle.size());
-    for(int i = 0 ; i < triangle.size(); i++){
-        d[i].resize(triangle[i].size());
-    }
-    
+    int n = triangle.size();
+    int m = triangle[n-1].size();
     d[0][0] = triangle[0][0];
-    for(int i = 1 ; i < triangle.size(); i++){
-        for(int j = 0 ; j < triangle[i].size(); j++){
-            if (j == 0)
-                d[i][j] = d[i-1][j] + triangle[i][j];
-            else if ( j == i)
-                d[i][j] = d[i-1][j-1] + triangle[i][j];
-            else
-                d[i][j] = max(d[i-1][j-1], d[i-1][j]) + triangle[i][j];
+    
+    for(int i = 1; i < n; i++){
+        for(int j = 0; j <= i; j++){
+            d[i][j] = triangle[i][j];
+            if(j == 0) d[i][j] += d[i-1][j];
+            else if(j == i) d[i][j] += d[i-1][j - 1];
+            else d[i][j] += max(d[i-1][j-1], d[i-1][j]);
         }
     }
     
-
-    int n = triangle.size();
-    answer = *max_element(d[n - 1].begin(),  d[n - 1].begin() + n); 
+    answer = *max_element(d[n-1], d[n-1] + m);
     return answer;
 }
+
+/*
+    d[i][j] i행 j열 값이 합 최대
+    
+    d[0][0] = triangle[0][0];
+    d[i][j] = max(d[i-1][j-1], d[i-1][j]) + triangle[i][j];
+    d[i][0] = d[i-1][0] +triangle[i][j];
+    d[i][j 끝] = d[i-1][j끝 -1 ] + triangle[i][j];
+*/
