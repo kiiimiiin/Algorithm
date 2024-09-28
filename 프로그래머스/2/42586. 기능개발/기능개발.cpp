dbcd892095082915e16758ea3f9542e073ddf997
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-09-28 11:32:59 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,30 +1,46 @@
-#include <string>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
-    vector<int> answer(1, 1);
-    int mxDate = ( 100 - progresses[0] ) / speeds[0] + 
-    ((100 - progresses[0]) % speeds[0] != 0);
+    vector<int> answer;
     
-    for(int i = 1; i < progresses.size(); i++){
-        int date = ( 100 - progresses[i] ) / speeds[i] +
-        ((100 - progresses[i]) % speeds[i] != 0);
-        
-        if(date <= mxDate) answer.back()++;
-        else{
-            mxDate = date;
-            answer.push_back(1);
-        }
-        
+    vector<int> dates;
+    for(int i = 0 ; i < speeds.size(); i++){
+        int date = (100 - progresses[i]) / speeds[i];
+        date += (100 - progresses[i]) % speeds[i] ? 1 : 0; 
+        dates.push_back(date);
     }
+    
+    for(int i = 0 ; i < dates.size(); i++)
+        cout << dates[i] << ' ';
+    
+    int num = dates[0];
+    int cnt = 1;
+    
+    for(int i = 1 ; i < dates.size(); i++){
+        if(num < dates[i]){
+            answer.push_back(cnt);
+            cnt = 1;
+            num = dates[i];
+        }else{
+            cnt++;
+        }
+    }
+    answer.push_back(cnt);
+
     return answer;
 }
 
 /*
-    7일 3일 9일
-    앞에서부터 걸리는 일자수 계산해나감
-    뒤에 나타나는게 걸리는 일자수보다 작거나 같을때 + 1, 클때 새로운 원소
-
+    각 일자를 계산 
+    
+    ex) 7 3 9 
+    
+    5 10 1 1 20 1
+    
+    -> 1 3 2 
+    
+    순회하며 현재값 보다 큰 수가 나타날 때 까지 cnt
+    -> 나타나면 cnt push 
+    
 */
