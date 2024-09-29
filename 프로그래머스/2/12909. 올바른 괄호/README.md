b737f4caffdc93c18836bc0f4df1413fd16520bb
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-09-29 14:39:08 +0900

    [level 2] Title: 올바른 괄호, Time: 0.47 ms, Memory: 3.87 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 3.95 MB, 시간: 0.50 ms
+메모리: 3.87 MB, 시간: 0.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 21일 22:32:58
+2024년 09월 29일 14:39:09
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -1,31 +1,34 @@
-#include <string>
-#include <stack>
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-bool solution(string s) {
-    ios::sync_with_stdio(0), cin.tie(0);
+bool solution(string s)
+{
     bool answer = true;
-    
-    stack<char> S; // 열린 괄호를 가진 stack
+
+    stack<char> st;
     
     for(auto c : s){
-        if(c == '('){
-            S.push(c);
-        }
-        else{
-            if(S.empty()){
+        if(c == '(') st.push(c);
+        else {
+            if(st.empty()){
                 answer = false;
                 break;
             }
-            else{
-                S.pop();
-            }
+            else
+                st.pop();
         }
     }
     
-    if(!S.empty()) answer = false;
+    if(!st.empty()) answer = false;
     
     return answer;
 }
+
+/*
+    열린괄호 푸쉬
+    닫힌괄호 ->
+    열린괄호 없으면 x
+    열린괄호 있으면 pop
+    
+    괄호남아있으면 x
+*/
