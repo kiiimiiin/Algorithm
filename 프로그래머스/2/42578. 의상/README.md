2e2cfb4eef3ec66e8b1c5807f45a8d3c4ed1fc08
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-10-01 17:53:24 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 3.68 MB, 시간: 0.02 ms
+메모리: 4.13 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 02일 11:48:00
+2024년 10월 01일 17:53:22
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -1,19 +1,28 @@
-#include <string>
-#include <vector>
-#include <unordered_map>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
     int answer = 1;
-    unordered_map<string,int> m;
+    unordered_map<string, int> m;
+    for(auto v : clothes)
+        m[v[1]]++;
     
-    for(int i = 0; i < clothes.size(); i++){
-        m[clothes[i][1]]++; 
-    }
     
-    for(auto e : m){
-        answer *= e.second + 1;
-    }
+    for(auto e : m)
+        answer *= e.second + 1; 
     
-    return answer - 1;
+    answer--;
+    
+    return answer;
 }
+
+/*
+ 
+    
+    x 모자1 모자2
+    x 선글라스1
+    
+    3 * 2 - 1 (아예 옷을 안 입는경우)
+    
+*/
