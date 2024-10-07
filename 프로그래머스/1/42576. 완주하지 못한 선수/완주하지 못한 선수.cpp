6c201246a17e112ced30fd7d13599b43ab61b3d7
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-10-07 17:45:05 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 69.59 ms, Memory: 32.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -1,27 +1,24 @@
-#include <unordered_set>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
-    unordered_multiset<string> ms(completion.begin(), completion.end());
-    for(auto x : participant){
-        auto it = ms.find(x); 
-        if(it == ms.end()){
-            answer = x;
+    unordered_map<string, int> m; 
+    
+    for(auto s : completion)
+        m[s]++;
+    
+    for(auto s : participant){
+        if(m.find(s) != m.end()){
+            m[s]--;
+            if(m[s] == 0)
+                m.erase(s);
+        } else {
+            answer = s;
             break;
-        } else{
-            ms.erase(it);
         }
     }
-    
+
     return answer;
 }
-
-/*
-    '동명이인 존재 가능'
-    완주자들을 해시셋으로 만든다.
-    참가자들을 순회하며 완주자에 없다면 완주 x 선수
-    
-*/
