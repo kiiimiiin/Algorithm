da10da994466a6ef09ee07cafd07dc5642e75c17
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-10-08 13:57:25 +0900

    [level 3] Title: 네트워크, Time: 0.05 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,39 +1,43 @@
-#include <string>
-#include <vector>
-#include <queue>
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
-    vector<bool> vis(n);
+    int vis[200] = {}; 
     queue<int> q;
    
-    for(int i = 0 ; i < n; i++){
-        if(!vis[i]){
-            answer++;
-            q.push(i);
-            vis[i] = true;
-            while(!q.empty()){
-                int cur = q.front(); q.pop();
-                for(int j = 0 ; j < n; j++){
-                    if(!vis[j] && computers[cur][j]){
-                        q.push(j);
-                        vis[j] = true;
-                    }
+    for(int idx = 0; idx < n; idx++){
+        if(vis[idx]) continue;
+        vis[idx] = 1;
+        q.push(idx); 
+        while(!q.empty()){
+            auto cur = q.front(); q.pop();
+            for(int idx = 0; idx < n; idx++){
+                if(vis[idx]) continue;
+                if(computers[cur][idx]){
+                    vis[idx] = 1;
+                    q.push(idx);
                 }
             }
         }
-    }    
+        answer++;
+    }
+    
+    
+    
+    
     return answer;
 }
 
-
-/*
-
-컴퓨터를 차례로 방문
-연결되어 있는 컴퓨터 bfs
-방문한 컴퓨터면 pass
+/*  
+    각 컴퓨터 순회, 
+    인덱스 0순회 : 1, 2 -> 1이랑 연결 vis
+    1 순회 : 연결된 것 없음 vis
+    --> 네트워크 처리
+    
+    1은 vis처리되어 넘김 
+    
+    2순회 : 연결된 것 없음 vis
+    --> 네트워크 처리
 
 */
