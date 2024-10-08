1d4f89961b4239239d631600e64f267a79bd6ea8
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-10-08 11:50:34 +0900

    [level 2] Title: 프로세스, Time: 0.02 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -1,49 +1,55 @@
 #include <bits/stdc++.h>
-#define IDX second
-#define PR first
+#define PRIORITY first
+#define LOCATION second
+
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
     int answer = 0;
-
-    deque<pair<int,int>> dq; 
-    for(int i = 0; i < priorities.size(); i++)
-        dq.push_back({priorities[i], i});
+    deque<pair<int,int>> dq;
+    priority_queue<int> pq(priorities.begin(), priorities.end());
+    
+    for(int idx = 0; idx < priorities.size(); idx++){
+        dq.push_back(make_pair(priorities[idx], idx));
+    }
     
     while(!dq.empty()){
         auto process = dq.front();
-        auto mxProcess = *max_element(dq.begin(), dq.end());
-        if(process.PR < mxProcess.PR){
+        auto mx = pq.top();
+    
+        if(process.PRIORITY < mx){
+            dq.pop_front();
             dq.push_back(process);
+        } else {
             dq.pop_front();
-        }else {
+            pq.pop();
             answer++;
-            if(process.IDX == location) break;
-            dq.pop_front();
+            if(process.LOCATION == location)
+                break; 
         }
-          
     }
     
     return answer;
-    
 }
 
 /*
-    priority 길이만큼 순회
-    front보다 큰 우선순위 존재 -> back으로
-    없으면 -> pop
+    덱의 맨앞원소보다 큰 우선순위가 있는 지 확인 있으면 다시 뒤로 넣음
+    없으면 해당 원소를 뺌, 이 때 원소가 기존 몇번 인덱스인지 알아야함.
+    인덱스가 location과 동일하면 종료
     
-    100 * 100
+    dq
     
-    2 1 3 2
-    A B C D
-    
-    B C D A
-    C D A B
-    D A B | C
-    A B | D
-    B | A 
-    | B
-    
+    pr : 2 1 3 2
+    idx: 0 1 2 3
+         a b c d
+         
+         b c d a
+         c d a b
+         d a b | c
+         a b | c d
+         b | c d a
+         c d a b
+         
+
     
 */
