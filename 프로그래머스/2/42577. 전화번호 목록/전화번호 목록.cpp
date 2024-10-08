c880b90eee8757de537ba636aa8d9971ee0c5e4f
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-10-08 11:15:24 +0900

    [level 2] Title: 전화번호 목록, Time: 283.33 ms, Memory: 48.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -3,23 +3,27 @@
 using namespace std;
 
 bool solution(vector<string> phone_book) {
-    bool answer = true;
-    unordered_set<string> s(phone_book.begin(), phone_book.end());
-    
+    bool answer = false;
+    unordered_set<string> set(phone_book.begin(), phone_book.end());
     sort(phone_book.begin(), phone_book.end());
     
-    for(auto hp : phone_book){
+    for(auto s : phone_book){
         string tmp = "";
-        for(int i = 0 ; i < hp.length() - 1; i++){
-            tmp += hp[i];
-            if(s.find(tmp) != s.end())
-                return false;
+        for(int i = 0; i < s.length() - 1; i++){
+            tmp.push_back(s[i]);
+            if( set.find(tmp) != set.end())
+                return answer;
         }
     }
     
-    return true;
+    answer = true;
+    return answer;
 }
 
 /*
-    O(2천만)
+    전화번호 오름차순 sort : 길이가 짧은 것부터 접두어 탐색하기 위해, 
+    
+    1235면 1, 12, 123 탐색 (자기자신 제외)
+    
+    O(2600만)
 */
