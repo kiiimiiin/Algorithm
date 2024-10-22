8cee92f03a43ff4e9ad9dba27e52fb464c967f76
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-10-22 10:34:03 +0900

    [level 2] Title: 타겟 넘버, Time: 3.84 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -3,30 +3,25 @@
 
 using namespace std;
 
-int solution(vector<int> numbers, int target) {
-    int answer = 0;
-    int n = numbers.size();
-    
-    for(int cse = 0 ; cse < ( 1 << n); cse++){
-        int brute = cse;
-        int val = 0;
-        for(int i = 0 ; i < n; i++){
-            if(brute % 2)
-                val += numbers[i];
-            else
-                val -= numbers[i];
-            brute /= 2;
-        }
-        
+void dfs(int k, int val, vector<int>& numbers, int& target, int& answer ){
+    if(k == numbers.size()){
         if(val == target)
             answer++;
+        return ;
     }
+    
+    dfs(k + 1, val + numbers[k], numbers, target, answer);
+    dfs(k + 1, val - numbers[k], numbers, target, answer);
+}
+
+int solution(vector<int> numbers, int target) {
+    int answer = 0;
+    
+    dfs(0, 0, numbers, target, answer);
     
     return answer;
 }
 
 /*
-    진법경우
-    숫자개수 n개
-    2^n 개의 전체 경우
+    dfs
 */
