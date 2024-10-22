d3e6db6c8d77cbf09d0d0e7f9fee9adfac25fd25
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-10-22 10:23:30 +0900

    [level 2] Title: 타겟 넘버, Time: 24.65 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -7,21 +7,26 @@ int solution(vector<int> numbers, int target) {
     int answer = 0;
     int n = numbers.size();
     
-    for(int cse = 0; cse < ( 1 << n ); cse++){
-        
+    for(int cse = 0 ; cse < ( 1 << n); cse++){
         int brute = cse;
         int val = 0;
-        for(int idx = 0 ; idx < n; idx++){
-            int isPlus = brute % 2;
-            brute /= 2;
-            if(isPlus)
-                val += numbers[idx];
+        for(int i = 0 ; i < n; i++){
+            if(brute % 2)
+                val += numbers[i];
             else
-                val -= numbers[idx];
+                val -= numbers[i];
+            brute /= 2;
         }
         
-        if(val == target) 
+        if(val == target)
             answer++;
     }
+    
     return answer;
 }
+
+/*
+    진법경우
+    숫자개수 n개
+    2^n 개의 전체 경우
+*/
