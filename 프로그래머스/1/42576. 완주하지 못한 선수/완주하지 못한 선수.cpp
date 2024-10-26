b1ef514b2955d87cdc074ca76e3e416f91ad6e1d
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-10-26 22:11:25 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 47.30 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -1,24 +1,27 @@
-#include <bits/stdc++.h>
-
+#include <string>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
-    unordered_map<string, int> m; 
-    
+    unordered_map<string, int> m;
     for(auto s : completion)
         m[s]++;
     
     for(auto s : participant){
-        if(m.find(s) != m.end()){
-            m[s]--;
-            if(m[s] == 0)
-                m.erase(s);
-        } else {
+        if(m.find(s) == m.end()){
             answer = s;
             break;
         }
+        m[s]--;
+        if(m[s] == 0)
+            m.erase(s);
     }
-
     return answer;
 }
+
+/* 
+    completion map만들어서
+    participant 순회하며 없는 이름 찾음
+*/
