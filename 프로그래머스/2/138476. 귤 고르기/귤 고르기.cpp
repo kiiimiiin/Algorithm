20cbd0a285418ff17ca3737aa522399a23e89073
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-10-27 13:39:07 +0900

    [level 2] Title: 귤 고르기, Time: 18.98 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
@@ -1,40 +1,32 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+
 using namespace std;
 
 int solution(int k, vector<int> tangerine) {
     int answer = 0;
-    unordered_map <int,int> m;
-    priority_queue<int> pq; // 최대힙
+    unordered_map<int,int> m;
+    priority_queue<int> pq;
     
     for(auto x : tangerine)
         m[x]++;
+    for(auto e : m)
+        pq.push(e.second);
     
-    for(auto x : m){
-        pq.push(x.second);
-    }
-    
-    while(k > 0){
+    while(!pq.empty()){
+        answer++;
         k -= pq.top();
         pq.pop();
-        answer++;
+        if(k <= 0) break;
     }
-    
     return answer;
 }
 
 /*
-    1 3 2 5 4 5 2 3
+    1 2 3 4 5
+    1 2 2 1 2
     
-    2 2 3 3 5 5 1 4
-    
-    2 2 2 1 1
-    
-    6 -2 -2 -2 => 3 카운트
-    6 -2 -2 -1 -1 => 4 카운트
-    
-    
-    2 3 5를 고르면 6개
-    갯수가 많은 순으로 내림차순 ( 종류별 갯수에 대해 알아야함 -> 해시셋)
-
-    O(n) + O(nlogn)
+    6개 택 -> 해당 종류 갯수가 가장 큰 원소부터
 */
