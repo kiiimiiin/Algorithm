cf0b76b067d2930e60206da66ea9d01e24929a0f
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-10-27 12:40:47 +0900

    [level 2] Title: 구명보트, Time: 1.69 ms, Memory: 4.94 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.cpp
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -1,31 +1,29 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <deque>
+
 using namespace std;
 
 int solution(vector<int> people, int limit) {
     int answer = 0;
+    sort(people.begin(), people.end());
+    deque<int> dq(people.begin(), people.end());
     
-    sort(people.begin(), people.end()); 
     
-    int idx = 0; // 다음에 태우고자하는 최소인덱스
-    
-    while(idx < people.size() ){
-        if(people.back() + people[idx] <= limit){
-            people.pop_back();
-            idx++;
-            answer++;
-        }else{
-            people.pop_back();
-            answer++;
-        }
-    }    
+    while(!dq.empty()){
+        if (dq.size() >= 2 && dq.front() + dq.back() <= limit){
+            dq.pop_front();
+            dq.pop_back();
+        } else
+            dq.pop_back();
+        answer++;
+    }
     return answer;
 }
 
 /*
-    오름차순 sort
-    *** 보트는 최대 2명씩 밖에 탈 수 없다!!!
-    
-    -최대 + 최소가 limit내이면 보트에 2명태운다
-    -limit을 넘어가면 최대 하나만 태운다
-
+    가장 작은 무게 + 큰 무게
+    -> 무게 제한 넘으면 큰무게만 구출
+    -> 안 넘으면 둘다 구출
 */
