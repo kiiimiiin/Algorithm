3cbc7955896008163134888e5565cb8e2bfcd775
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-10-27 11:26:03 +0900

    [level 3] Title: 네트워크, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,40 +1,27 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
 using namespace std;
+bool vis[202] = {};
 
-void dfs(int cur, int vis[200], vector<vector<int>>& computers, int n){
-    for(int idx = 0; idx < n; idx++){
-        if(vis[idx]) continue;
-        if(computers[cur][idx]){
-            vis[idx] = 1;
-            dfs(idx, vis, computers, n); 
+void dfs(int cur, vector<vector<int>>& computers){
+    for(int j = 0; j < computers.size(); j++){
+        if(vis[j]) continue;
+        if(computers[cur][j]){
+            vis[j] = true;
+            dfs(j, computers);
         }
     }
 }
-
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
-    int vis[200] = {}; 
-
-    for(int idx = 0; idx < n; idx++){
-        if(vis[idx]) continue;
-        vis[idx] = 1;
-        
-        dfs(idx, vis, computers, n);    
+    
+    
+    for(int i = 0; i < n; i++){
+        if(vis[i]) continue;
         answer++;
+        vis[i] = true;
+        dfs(i, computers);
     }
     
     return answer;
 }
-
-/*  
-    각 컴퓨터 순회, 
-    인덱스 0순회 : 1, 2 -> 1이랑 연결 vis
-    1 순회 : 연결된 것 없음 vis
-    --> 네트워크 처리
-    
-    1은 vis처리되어 넘김 
-    
-    2순회 : 연결된 것 없음 vis
-    --> 네트워크 처리
-
-*/
