18dc961f43d81d208bcf43a561bbbba95f1717c4
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-10-28 10:35:40 +0900

    [level 2] Title: 주식가격, Time: 26.28 ms, Memory: 26.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -1,25 +1,30 @@
-#include <iostream>
-#include <stack>
-#include <utility>
-#include <vector>
+#include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
     vector<int> answer(prices.size());
-    stack<int> S; // 인덱스
+    stack<int> s;
+      
     
     for(int i = 0; i < prices.size(); i++){
         
-        while(!S.empty() && prices[S.top()] > prices[i]){
-            answer[S.top()] = i - S.top();
-            S.pop();
+        while(!s.empty() && prices[s.top()] > prices[i]){
+            answer[s.top()] = i - s.top();
+            s.pop();
         }
         
-        answer[i] = ( prices.size() - 1) - i; 
-        S.push(i);
-        
+        s.push(i);
+        answer[i] = prices.size() - 1 - i;
     }
     
     return answer;
 }
+
+/*
+    스택에서 현재값이 top prices보다 떨어지는 순간
+    그전의 인덱스들에 대해 현재값보다 작은경우, 떨어지도록
+    
+    그렇지 않으면 끝까지 안떨어지는것
+*/
