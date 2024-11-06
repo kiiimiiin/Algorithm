1c3d2a3e29b96be4a78da70bc6f3fa8633f09145
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-11-06 17:29:03 +0900

    [level 1] Title: 삼총사, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.c
+++ 프로그래머스/1/131705. 삼총사/삼총사.c
@@ -1,34 +1,19 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-bool vis[14];
-int idx_cse[4];
-int n, answer;
 
-void dfs(int k, int number[]){
-    if( k == 3){
-        int sum = 0;
-        for(int i = 0 ; i < 3; i++){
-            //printf("%d ", idx_cse[i]);
-            sum += number[idx_cse[i]];
-        }
-        //printf("\n");
-    
-        if(sum == 0) answer++;
-        return ;
-    }
-    
-    for(int i = 0; i < n; i++){
-        if(vis[i] || ( k > 0 && i <= idx_cse[k-1] )) continue;
-        vis[i] = true;
-        idx_cse[k] = i;
-        dfs(k + 1, number);
-        vis[i] = false;
-    }
-}
 // number_len은 배열 number의 길이입니다.
 int solution(int number[], size_t number_len) {
-    n = number_len; 
-    dfs(0,number);
+    int answer = 0;
+    
+    for(int i = 0; i < number_len; i++){
+        for(int j = i + 1; j < number_len; j++){
+            for(int k = j + 1; k < number_len; k++){
+                if(number[i] + number[j] + number[k] == 0)
+                    answer++;
+            }
+        }
+    }
+    
     return answer;
 }
