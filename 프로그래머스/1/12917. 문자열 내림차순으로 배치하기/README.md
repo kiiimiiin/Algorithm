b5daf58573b3da07ee02950ad4129e093e4c81f2
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-11-10 14:24:27 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.05 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/README.md
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 내림차순으로 배치하기 - 12917 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12917) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12917?language=c) 
 
 ### 성능 요약
 
-메모리: 4.54 MB, 시간: 0.18 ms
+메모리: 4.16 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 05일 22:09:31
+2024년 11월 10일 14:24:26
 
 ### 문제 설명
 
--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.c
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.c
@@ -2,21 +2,17 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int compare(char *a, char *b){
+    return *b - *a;
+}
 
 char* solution(const char* s) {
-
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     size_t len = strlen(s);
     char* answer = (char*)malloc(len + 1);
     strcpy(answer, s);
+    qsort(answer, len, sizeof(char), compare);
     
-    for(int i = 0; i < len; i++){
-        for(int j = i + 1; j < len; j++){
-            if(answer[i] < answer[j]){
-                char tmp = answer[i];
-                answer[i] = answer[j];
-                answer[j] = tmp;
-            }
-        }
-    }
     return answer;
 }
