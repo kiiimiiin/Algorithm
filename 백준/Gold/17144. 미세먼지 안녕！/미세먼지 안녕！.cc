0f0de6cca540c0db58528c59fe539acc48290310
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2025-03-23 18:19:39 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 32 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.cc
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.cc
@@ -1,26 +1,39 @@
 #include <iostream>
-#define X first; 
-#define Y second;
-using namespace std;
-int dx[4] = { -1, 0, 1, 0 };
-int dy[4] = { 0, 1, 0, -1 }; 
-int board[52][52]; 
-int r, c, t, head, tail; 
+using namespace std; 
 
-bool OOB(int x, int y) {
-	return (x < 0 || x >= r || y < 0 || y >= c);
+int r, c, t;
+int board[52][52]; 
+
+int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, 1, 0, -1 }; 
+
+struct Machine {
+	int top, bottom;
+};
+
+Machine m;
+
+bool OOB(int nx, int ny) {
+	return nx < 0 || nx >= r || ny < 0 || ny >= c; 
 }
-void spread() {
-	int cBoard[52][52] = {} ;
+
+bool IsMachine(int nx, int ny) {
+	return (nx == m.bottom || nx == m.top) && ny == 0;
+}
+
+void Spread() {
+	int tmp[52][52] = {};
+
 	for (int i = 0; i < r; i++) {
 		for (int j = 0; j < c; j++) {
 			if (board[i][j] > 0) {
+				int val = board[i][j] / 5;
 				for (int dir = 0; dir < 4; dir++) {
 					int nx = i + dx[dir];
 					int ny = j + dy[dir];
-					if (OOB(nx, ny) || board[nx][ny] == -1) continue;
-					cBoard[nx][ny] += board[i][j] / 5;
-					cBoard[i][j] -= board[i][j] / 5; 
+					if (OOB(nx, ny) || (IsMachine(nx, ny))) continue;
+					tmp[nx][ny] += val;
+					tmp[i][j] -= val; 
 				}
 			}
 		}
@@ -28,47 +41,40 @@ void spread() {
 
 	for (int i = 0; i < r; i++) {
 		for (int j = 0; j < c; j++) {
-			board[i][j] += cBoard[i][j];
+			board[i][j] += tmp[i][j];
 		}
 	}
 }
 
-void clean() {
-	
-	// x : head - 1을 시작으로 수를 head쪽으로 밀자
-	int x, y, nx, ny, dir; 
-	int hdx[4] = { -1, 0, 1, 0 };
-	int hdy[4] = { 0, 1, 0, -1 };
-	x = head - 1; 
-	y = 0; 
-	dir = 0;
-	nx = ny = 0; 
-
-	while (1) {
-		nx = x + hdx[dir];
-		ny = y + hdy[dir];
-		if (nx == head && ny == 0) break;
-		if (nx < 0 || ny >= c || nx > head) {
-			dir = (dir + 1) % 4; 
-			continue; 
-		}
-		board[x][y] = board[nx][ny]; 
-		x = nx; y = ny; 
-	}
-	board[x][y] = 0; 
-
-	int tdx[4] = { 1, 0, -1, 0 };
+void Wind() {
+	int tdx[4] = {-1, 0, 1, 0};
 	int tdy[4] = { 0, 1, 0, -1 };
-	x = tail + 1;
+	int x = m.top - 1; 
+	int y = 0;
+	int dir = 0;
+	
+	while (!(x == m.top && y == 1)) {
+		int nx = x + tdx[dir];
+		int ny = y + tdy[dir];
+		if (OOB(nx, ny) || nx > m.top) {
+			dir = (dir + 1) % 4;
+			continue;
+		}
+		board[x][y] = board[nx][ny];
+		x = nx; y = ny;
+	}
+	board[x][y] = 0;
+
+	int bdx[4] = { 1, 0, -1, 0 };
+	int bdy[4] = { 0, 1, 0, -1 };
+	x = m.bottom + 1;
 	y = 0;
 	dir = 0;
-	nx = ny = 0;
 
-	while (1) {
-		nx = x + tdx[dir];
-		ny = y + tdy[dir];
-		if (nx == tail && ny == 0) break;
-		if (nx >= r || ny >= c || nx < tail) {
+	while (!(x == m.bottom && y == 1)) {
+		int nx = x + bdx[dir];
+		int ny = y + bdy[dir];
+		if (OOB(nx, ny) || nx < m.bottom) {
 			dir = (dir + 1) % 4;
 			continue;
 		}
@@ -82,36 +88,34 @@ int getAns() {
 	int ret = 0;
 	for (int i = 0; i < r; i++) {
 		for (int j = 0; j < c; j++) {
-			if (board[i][j] == -1) continue;
-			ret += board[i][j]; 
+			if (board[i][j] > 0)
+				ret += board[i][j];
 		}
 	}
+
 	return ret; 
 }
 
-int main(void) {
-	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+int main() {
+	ios::sync_with_stdio(0), cin.tie(0);
 
-	cin >> r >> c >> t; 
+	cin >> r >> c >> t;
 
 	for (int i = 0; i < r; i++) {
 		for (int j = 0; j < c; j++) {
-			int n;
-			cin >> n;
-			board[i][j] = n;
-			if (n == -1) {
-				tail = i; 
-			}
+			cin >> board[i][j];
+			if (board[i][j] == -1)
+				m.bottom = i;
 		}
 	}
 
-	head = tail - 1;
+	m.top = m.bottom - 1;
 
 	while (t--) {
-		spread();
-		clean(); 
+		Spread();
+		Wind();
 	}
 
-	int ans = getAns(); 
-	cout << ans;
+	int ans = getAns();
+	cout << ans; 
 }
