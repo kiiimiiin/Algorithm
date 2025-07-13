0a44f6869eaa7ed18706bae72d34be6736f85443
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2025-07-13 21:19:15 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.28 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,37 +1,37 @@
-#include <bits/stdc++.h>
+#include<vector>
+#include<queue>
 #define X first
 #define Y second
 using namespace std;
-const int dx[4] = {1, 0, -1, 0};
-const int dy[4] = {0, 1, 0, -1}; 
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
 int dist[102][102];
+
 
 int solution(vector<vector<int> > maps)
 {
     int n = maps.size();
     int m = maps[0].size();
+    int answer = 0;
     queue<pair<int,int>> q;
-    for(int i = 0; i < n ; i++) 
-        fill(dist[i], dist[i] + m, -1);
+    for(int i = 0 ; i < n; i++) fill(dist[i] , dist[i] + m, -1);
     
+    q.push({0,0});
     dist[0][0] = 0;
-    q.push({0, 0});
     
     while(!q.empty()){
-        auto cur = q.front(); q.pop(); 
+        auto cur = q.front(); q.pop();
         for(int dir = 0 ; dir < 4; dir++){
             int nx = cur.X + dx[dir];
             int ny = cur.Y + dy[dir];
             if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-            if(maps[nx][ny] == 0 || dist[nx][ny] >= 0) continue;
-            dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+            if(dist[nx][ny] >= 0 || maps[nx][ny] != 1) continue;
             q.push({nx,ny});
+            dist[nx][ny] = dist[cur.X][cur.Y] + 1;
         }
     }
-    
-    if(dist[n-1][m-1] == -1)
-        return -1;
-    else
-        return dist[n-1][m-1] + 1;
-    
+
+        
+    answer = (dist[n-1][m-1] == -1 ? -1 : dist[n-1][m-1] + 1 );
+    return answer;
 }
