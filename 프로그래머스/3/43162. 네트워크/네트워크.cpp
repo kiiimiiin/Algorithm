0ede658e87326d0745fbadb42fc531428dd00e15
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2025-07-20 18:50:06 +0900

    [level 3] Title: 네트워크, Time: 0.05 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,27 +1,32 @@
 #include <string>
 #include <vector>
-using namespace std;
-bool vis[202] = {};
+#include <queue>
 
-void dfs(int cur, vector<vector<int>>& computers){
-    for(int j = 0; j < computers.size(); j++){
-        if(vis[j]) continue;
-        if(computers[cur][j]){
-            vis[j] = true;
-            dfs(j, computers);
-        }
-    }
-}
+using namespace std;
+
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
+    bool vis[202] = {};
+    queue<int> q;
     
     
-    for(int i = 0; i < n; i++){
+    for(int i = 0 ; i <  computers.size(); i++){
         if(vis[i]) continue;
         answer++;
+        q.push(i);
         vis[i] = true;
-        dfs(i, computers);
+        
+        while(!q.empty()){
+            int idx = q.front(); q.pop();
+            for(int j = 0 ; j < computers[idx].size(); j++){
+                if(vis[j] || computers[idx][j] == 0) continue;
+                vis[j] = true;
+                q.push(j);        
+            }   
+        }   
     }
     
     return answer;
 }
+
+// a-b-c a-c 정보교환 o 
