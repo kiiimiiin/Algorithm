42252f8683ff6522cb218e47d6c669305d3808b2
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2025-07-20 14:35:32 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -1,28 +1,17 @@
-#include <bits/stdc++.h>
-
+#include <string>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
     int answer = 1;
-    unordered_map<string, int> m;
-    for(auto v : clothes)
-        m[v[1]]++;
+    unordered_map<string,int> m;
+    for(auto cloth : clothes)
+        m[cloth[1]]++;
     
-    
-    for(auto e : m)
-        answer *= e.second + 1; 
-    
+    for(auto cse : m)
+        answer *= cse.second + 1; 
+
     answer--;
-    
     return answer;
 }
-
-/*
- 
-    
-    x 모자1 모자2
-    x 선글라스1
-    
-    3 * 2 - 1 (아예 옷을 안 입는경우)
-    
-*/
