2370ca397ab18c18e56d87f1c58441d1cb7dc125
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2025-07-20 13:01:01 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 8.78 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -1,48 +1,51 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <deque>
+#include <iostream>
+
 using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
     int answer = 0;
-    int n = truck_weights.size();
-    int weight_on_bridge = 0;
-    deque<int> dq(truck_weights.begin(), truck_weights.end()); // 대기 트럭
-    queue<pair<int,int>> q; // 다리를 건너는 트럭 (무게, 시간)
+
+    int idx = 0; // 대기 트럭 인덱스
+    int total_weight = 0; // 현재 무게
+    deque<pair<int,int>> bridge;
     
-    while(1){
-        answer++; 
+    while(idx != truck_weights.size()){
+        answer++;
         
-        if(!q.empty() && answer == q.front().second){
-            weight_on_bridge -= q.front().first;
-            q.pop();
-        }
+        for(int i = 0 ; i < bridge.size(); i++)
+            bridge[i].second--;
         
-        if(!dq.empty()){
-            int nxt_weight = dq.front();
-            if(q.size() + 1 <= bridge_length && weight_on_bridge + nxt_weight <= weight){
-                q.push(make_pair(nxt_weight, answer + bridge_length));
-                weight_on_bridge += nxt_weight;
-                dq.pop_front();
-            }    
-        }
-        
-        if(dq.empty()) {
-            answer = q.back().second;
-            break;
-        }
-        
+        if(!bridge.empty() && bridge.front().second == 0){
+            total_weight -= bridge.front().first;
             
+            bridge.pop_front();
+        }
+            
+        
+        if( total_weight + truck_weights[idx] <= weight && bridge.size() < bridge_length){
+            total_weight += truck_weights[idx];
+            bridge.push_back({truck_weights[idx], bridge_length});
+            cout << truck_weights[idx] << ',' << idx << ' ';
+            idx++;
+        } 
+        
+
+        
     }
+    
+    if(!bridge.empty()) answer += bridge.back().second;
+    
     return answer;
+    
 }
 
-/*
-   다리에 트럭이 다 건널 시간이라면 다리에서 트럭을 뺀다.
-   
-   다리무게가 견딜수 있으면, 대기트럭에서 다리로 트럭을 옮긴다.
-            견딜 수 없으면, 옮기지 않는다.
-    
-    트럭을 다리에 옮길때 트럭이 빠지는 시간을 고려해서 같이 넣는다. (무게, 시간)
-    
-    
+// bridge_length 만큼의 시간 계산
+// weight 제한
 
-*/
+// 1 : 7 2
+// 2 : 7 1 
+// 
+
