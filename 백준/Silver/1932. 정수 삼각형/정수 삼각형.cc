33e826c1e3276e40ad8ded17a5ad595a8227167b
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2025-07-24 21:28:28 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 3004 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,33 +1,30 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int d[502][502];
-int n, x;
+int d[502][502]; // fibonacci(i)일 때 0, 1 출력 수
 
-int main(){
+int main()
+{
     ios::sync_with_stdio(0), cin.tie(0);
-    
+    int n;
     cin >> n;
     
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= i; j++){
-            cin >> x;
-            d[i][j] = max(d[i-1][j-1], d[i-1][j]) + x; 
+            int num;
+            cin >> num;
+            d[i][j] = max( d[i-1][j] , d[i-1][j-1] ) +  num;
         }
     }
     
-    cout << *max_element(d[n] + 1, d[n] + n + 1); 
+    cout << *max_element(d[n] , d[n] + n + 1);
+    return 0;
 }
 
-
 /*
-    d[i][j] : i번째 행에서 j열을 택했을 때까지의 최대값
-
-    d[1][1] = 7
+    d[4][3] = d[3][3] + d[3][2]
     
-    d[2][1] = max(d[1][0] ,d[1][1] ) + num[i][j];
-    d[2][2] = max(d[1][1] ,d[1][2] ) + num[i][j];
+    d[i][j] : i번째 층에서 j번째 수를 택했을 때 까지의 경로 합
+    d[i][j] = d[i-1][j]  + d[i-1][j-1]; 
     
-    d[i][j] = d[i-1][j-1] + d[i-1][j];
-     
 */
