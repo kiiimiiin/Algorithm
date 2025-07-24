4c5c605e8618831eda5a95502a319100e84af68e
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2025-07-24 22:02:01 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.cc
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -1,29 +1,22 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int n;
-long long d[92][2]; 
+long long d[92][2]; // i번째 자리가 0, 1 일때 이친수 개수
 
-int main(){
+int main()
+{
     ios::sync_with_stdio(0), cin.tie(0);
-    cin >> n; 
+    int n;
+    cin >> n;
     
     d[1][0] = 0;
-    d[1][1] = 1;
+    d[1][1] = 1; 
     
     for(int i = 2; i <= n; i++){
         d[i][0] = d[i-1][0] + d[i-1][1];
         d[i][1] = d[i-1][0];
     }
     
-    cout << d[n][0] + d[n][1]; 
-    
+    cout << d[n][0] + d[n][1];
+    return 0;
 }
-
-/*
-    d[i][j] i번째 짜리가 j (0,1) 일때 이친수개수
-    
-    d[i][0] = d[i-1][0] + d[i-1][1]
-    d[i][1] = d[i-1][0]
-    
-*/
