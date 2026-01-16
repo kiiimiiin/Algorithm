5c3ed31ee27ba4a06d67ee97e739477d21803b90
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2026-01-16 22:07:19 +0900

    [Gold I] Title: 청소년 상어, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/19236. 청소년 상어/청소년 상어.cc
+++ 백준/Gold/19236. 청소년 상어/청소년 상어.cc
@@ -1,121 +1,93 @@
 #include <iostream>
-const int dx[8] = { -1, -1, 0, 1, 1, 1, 0, -1 };
-const int dy[8] = { 0, -1, -1, -1, 0, 1, 1, 1 };
+#include <vector>
+#include <algorithm>
 using namespace std;
-int board[4][4]; // 물고기 보드
-int mx = -0x7f7f7f7f;
+int dx[8] = { -1,-1,0,1,1,1,0,-1 };
+int dy[8] = { 0,-1,-1,-1,0,1,1,1 };
+int board[4][4];
 
 struct Fish {
-	int x, y;
-	int dir;
-	bool isLived;
+	int x, y, dir;
+	bool isLived = false;
 };
 
-Fish fish[17];
+Fish fishes[17];
 
+void MoveFish(int sx, int sy) {
+	for (int idx = 1; idx <= 16; idx++) {
+		if (fishes[idx].isLived == false) continue;
+		
+		for(int dd = 0 ; dd < 8; dd++){
+			int nd = (fishes[idx].dir + dd) % 8; 
+			int nx = fishes[idx].x + dx[nd];
+			int ny = fishes[idx].y + dy[nd];			
+			if (nx < 0 || nx >= 4 || ny < 0 || ny >= 4) continue;
+			if (nx == sx && ny == sy) continue;
+			// temp or fish
+			int cx = fishes[idx].x;
+			int cy = fishes[idx].y;
 
-void Copy(int desc[4][4], int src[4][4], Fish fdesc[17], Fish fsrc[17]) {
+			fishes[idx].x = nx;
+			fishes[idx].y = ny;
+			fishes[idx].dir = nd;
+			fishes[board[nx][ny]].x = cx;
+			fishes[board[nx][ny]].y = cy;
+			swap(board[nx][ny], board[cx][cy]);
+			break;
+		}
+	}
+
+	return; 
+}
+
+void Copy(int desc[4][4], int src[4][4], Fish fdes[17], Fish fsrc[17]) {
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
 			desc[i][j] = src[i][j];
 		}
 	}
 
-	for (int i = 1; i <= 16; i++)
-		fdesc[i] = fsrc[i];
-}
-bool OOB(int x, int y) {
-	return x < 0 || x >= 4 || y < 0 || y >= 4;
-}
-
-
-void MoveFish(int sx, int sy) {
-	for (int i = 1; i <= 16; i++) {
-		if (!fish[i].isLived) continue;
-
-		int nx, ny, nd, dd;
-
-		for (dd = 0; dd < 8; dd++) {
-			nd = (fish[i].dir + dd) % 8;
-			nx = fish[i].x + dx[nd];
-			ny = fish[i].y + dy[nd];
-			if (OOB(nx, ny) || (nx == sx && ny == sy)) continue;
-			break;
-		}
-
-
-		if (dd == 8) continue;
-
-		if (board[nx][ny] >= 1 && board[nx][ny] <= 16) {
-			Fish f = fish[i];
-
-			fish[i].x = nx;
-			fish[i].y = ny;
-			fish[i].dir = nd;
-			fish[board[nx][ny]].x = f.x;
-			fish[board[nx][ny]].y = f.y;
-
-			board[f.x][f.y] = board[nx][ny];
-			board[nx][ny] = i;
-
-		}
-		else if (board[nx][ny] == 0) {
-			Fish f = fish[i];
-
-			fish[i].x = nx;
-			fish[i].y = ny;
-			fish[i].dir = nd;
-
-			board[f.x][f.y] = 0;
-			board[nx][ny] = i;
-		}
+	for (int i = 0; i < 17; i++) {
+		fdes[i] = fsrc[i];
 	}
+
+	return;
 }
+void dfs(int sx, int sy, int sum, int& ans) {
 
-void dfs(int eatten, int sx, int sy, int sd) {
-
-	// 1. 상어자리 물고기 섭취
-	eatten += board[sx][sy];
-	sd = fish[board[sx][sy]].dir;
-	fish[board[sx][sy]].isLived = false;
+	int sd = fishes[board[sx][sy]].dir;
+	fishes[board[sx][sy]].isLived = false;
+	sum += board[sx][sy]; ans = max(ans, sum);
 	board[sx][sy] = 0;
-	mx = max(mx, eatten);
 
-
-	// 2. 물고기 이동
 	MoveFish(sx, sy);
-
-	// 3. 상어 이동
-	int tmp[4][4];
-	Fish ftmp[17];
-	Copy(tmp, board, ftmp, fish);
+	
+	int temp[4][4] = {};
+	Fish ftemp[17] = {};
+	
+	Copy(temp, board, ftemp, fishes);
 
 	for (int p = 1; p <= 3; p++) {
 		int nx = sx + p * dx[sd];
 		int ny = sy + p * dy[sd];
-		if (OOB(nx, ny) || board[nx][ny] == 0) continue;
-
-		dfs(eatten, nx, ny, sd);
-
-		Copy(board, tmp, fish, ftmp);
+		if (nx < 0 || nx >= 4 || ny < 0 || ny >= 4) continue; 
+		if (board[nx][ny] == 0) continue; 
+		dfs(nx, ny, sum, ans);
+		Copy(board, temp, fishes, ftemp);
 	}
-
 }
 
-
-int main() {
-	ios::sync_with_stdio(0), cin.tie(0);
-
-
+int main()
+{
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
-			int dir;
-			cin >> board[i][j] >> dir;
-			fish[board[i][j]] = { i, j, dir - 1, true };
+			int idx, dir;
+			cin >> idx >> dir;
+			board[i][j] = idx;
+			fishes[idx] = { i, j, dir - 1, true};
 		}
 	}
-
-	dfs(0, 0, 0, 0);
-	cout << mx;
-
+	int ans = -0x7f7f7f7f;
+	dfs(0, 0, 0, ans);
+	std::cout << ans;
 }
