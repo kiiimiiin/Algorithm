13b2ad9bdb8ebab9b89d45981aabce550fb8e1ea
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2026-03-17 10:06:23 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -6,31 +6,19 @@ using namespace std;
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
     vector<int> dates;
+    for(int i = 0; i < progresses.size(); i++){
+        int date = ( 100 - progresses[i] ) / speeds[i] +
+            (( 100 - progresses[i] ) % speeds[i] ? 1 : 0) ;
+        
+        if(answer.empty() || date > dates.back()){
+            answer.push_back(1);
+            dates.push_back(date);
+        }
+        else if(date <= dates.back()){
+            answer.back()++;
+        }
     
-    for(int i = 0 ; i < progresses.size(); i++){
-        int date = ( 100 - progresses[i] ) / speeds[i] + 
-            ( ( 100 - progresses[i] ) % speeds[i] ? 1 : 0 ) ;    
-        dates.push_back(date);
+        
     }
-    
-    int cnt = 0;
-    int lastDate = 0;
-    for(int i = 0 ; i < dates.size(); i++){
-        if(i == 0){
-            lastDate = dates[i];
-            cnt++;
-        }
-        else if(dates[i] <= lastDate){
-            cnt++;
-        }
-        else{
-            answer.push_back(cnt);
-            lastDate = dates[i];
-            cnt = 1;
-        }
-    }
-            
-    answer.push_back(cnt);
-    
     return answer;
 }
